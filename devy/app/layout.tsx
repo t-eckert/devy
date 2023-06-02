dfe14c3fad45a7d5c9987da29de63e37f934a02c
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-06-01 23:33:38 -0400

    Stub out homepage layout

--- devy/app/layout.tsx
+++ devy/app/layout.tsx
@@ -15,7 +15,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="px-2 py-1 w-full flex flex-row items-center justify-between">
+          <div className="flex flex-row gap-1.5 items-baseline">
+            <h1 className="text-sm font-medium">Devy</h1>
+            <span className="px-1 py-0.25 text-xs rounded-full border border-slate-700 font-medium">
+              Pre-release
+            </span>
+          </div>
+          <div>
+            <button className="px-2 py-0.5 text-sm font-medium rounded-xl border border-slate-700">
+              Sign in with GitHub
+            </button>
+          </div>
+        </header>
+        {children}
+        <footer></footer>
+      </body>
     </html>
   )
 }
--- devy/app/page.tsx
+++ devy/app/page.tsx
@@ -1,3 +1,40 @@
 export default function Home() {
-  return <main></main>
+  return (
+    <>
+      <main className="mx-auto mt-12 w-full max-w-6xl flex flex-row justify-between">
+        <section className="flex flex-col items-start gap-2">
+          <button className="px-2 py-0.5 border border-slate-700 shadow rounded-xl font-medium w-32 flex justify-start hover:pl-4 transition-all">
+            Popular
+          </button>
+          <button className="px-2 py-0.5 rounded-xl w-32 flex justify-start hover:pl-4 transition-all">
+            New
+          </button>
+        </section>
+        <section className="w-full max-w-xl flex flex-col gap-2">
+          <div>
+            <h2 className="font-medium">Post title</h2>
+            <p>Post description</p>
+          </div>
+          <div>
+            <h2 className="font-medium">Post title</h2>
+            <p>Post description</p>
+          </div>
+          <div>
+            <h2 className="font-medium">Post title</h2>
+            <p>Post description</p>
+          </div>
+          <div>
+            <h2 className="font-medium">Post title</h2>
+            <p>Post description</p>
+          </div>
+          <div>
+            <h2 className="font-medium">Post title</h2>
+            <p>Post description</p>
+          </div>
+        </section>
+        <section>Secondary info</section>
+      </main>
+      <footer></footer>
+    </>
+  )
 }
