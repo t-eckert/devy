6ed3d5193c0c1c69aa09d0757c4e416003725f3d
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-06-03 21:18:14 -0400

    Fix props to Feeds

--- devy/app/Feeds.tsx
+++ devy/app/Feeds.tsx
@@ -3,11 +3,13 @@
 // TODO: I'd like to give this component a better name.
 
 import Tab from "@/models/Tab"
+import Feed from "@/models/Feed"
 
 interface Props {
   tabs: Tab[]
+  feeds: Feed[]
 }
 
-export default function Feeds() {
+export default function Feeds({ tabs, feeds }: Props) {
   return <div>Feeds</div>
 }
