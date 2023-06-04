7de7cc1cf4878fcd43aaf36c31c7ca87488d7f17
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-06-04 18:31:19 -0400

    Add to denylisted blog names

--- devy/lib/constants.ts
+++ devy/lib/constants.ts
@@ -1,1 +1,9 @@
-export const DENYLISTED_BLOG_NAMES = ["about", "docs", "settings", "profile"]
+// These values cannot be used as blog names as they will conflict with existing routes.
+export const DENYLISTED_BLOG_NAMES = [
+	"about",
+	"docs",
+	"settings",
+	"profile",
+	"rss",
+	"uploads",
+]
