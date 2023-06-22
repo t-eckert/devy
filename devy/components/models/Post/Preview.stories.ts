352e4f9dd71f15818fedaca8c04472c5e43b5f95
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-06-21 22:08:16 -0400

    Add Preview story

--- devy/components/models/Post/Preview.stories.ts
+++ devy/components/models/Post/Preview.stories.ts
@@ -1,20 +1,9 @@
 import type { Meta, StoryObj } from "@storybook/react"
 
 import Preview from "./Preview"
+import posts from "@/fixtures/posts"
 
-const post = {
-  slug: "hello-world",
-  blog: {
-    slug: "blog",
-  },
-  author: {
-    username: "j-doe",
-    displayName: "John Doe",
-  },
-  published: "2021-01-01T00:00:00.000Z",
-  title: "Hello World",
-  tags: ["hello", "world"],
-}
+const post = posts[0]
 
 const meta: Meta<typeof Preview> = {
   title: "Components/Models/Post/Preview",
