01c4a550914d39e1d5feec18a89a8ccd59a78ec7
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-07-24 22:19:16 -0400

    Add basic Poem app

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use poem::{get, handler, listener::TcpListener, web::Path, IntoResponse, Route, Server};
+
+#[handler]
+fn hello(Path(name): Path<String>) -> String {
+    format!("hello: {}", name)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let app = Route::new().at("/hello/:name", get(hello));
+    Server::new(TcpListener::bind("127.0.0.1:3000"))
+        .run(app)
+        .await
 }
