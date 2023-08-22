cfdba4090cb364e7ea3a502f093bbf72a827ae7d
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-08-22 00:21:38 -0400

    Add changelog page

--- frontend/app/changelog/page.tsx
+++ frontend/app/changelog/page.tsx
@@ -1,7 +1,11 @@
-export default function Changelog() {
+export default async function Changelog() {
+  const res = await fetch("https://raw.githubusercontent.com/t-eckert/devy/main/CHANGELOG.md")
+  const text = await res.text()
+
+
   return (
     <main className="mx-auto my-4 flex flex-col sm:flex-row px-2 w-full max-w-6xl gap-4 sm:gap-2">
-      Changelog
+      {text}
     </main>
   )
 }
