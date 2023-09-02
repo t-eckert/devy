aae2035901a8c27dd1160000704762d3a30956ad
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-09-02 11:10:32 -0400

    Add 'Sign in' button

--- frontend/components/dynamic/Nav/Nav.tsx
+++ frontend/components/dynamic/Nav/Nav.tsx
@@ -4,18 +4,16 @@ import { Link } from "@/components/elements"
 
 import Menu from "./Menu"
 
-const signInImplemented = false
 
 export default function Nav() {
 	return (
 		<nav className="flex flex-row border border-zinc-700 rounded-full bg-zinc-800">
-			{signInImplemented && (
-				<Link href="/login">
-					<div className="pl-2 pr-2 py-0.5 flex flex-row gap-2 items-center">
-						<GitHubLogoIcon className="w-4 h-4" />
-						<span className="text-sm font-medium">Sign in</span>
-					</div>
-				</Link>)}
+			<Link href="/api/login">
+				<div className="pl-2 pr-2 py-0.5 flex flex-row rounded-l-full gap-2 items-center border-r border-r-zinc-700 hover:bg-zinc-700 transition-all">
+					<GitHubLogoIcon className="w-4 h-4" />
+					<span className="text-sm font-medium">Sign in</span>
+				</div>
+			</Link>
 			<Menu />
 		</nav>
 	)
