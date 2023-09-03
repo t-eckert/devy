061bc3a890751ea491c70dc6fb6760388f79aebd
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-09-03 13:33:03 -0400

    Receive JWT on frontend

--- frontend/components/dynamic/Nav/Nav.tsx
+++ frontend/components/dynamic/Nav/Nav.tsx
@@ -8,7 +8,7 @@ import Menu from "./Menu"
 export default function Nav() {
 	return (
 		<nav className="flex flex-row border border-zinc-700 rounded-full bg-zinc-800">
-			<Link href="/api/login">
+			<Link href="/api/auth/login?redirect=/" prefetch={false}>
 				<div className="pl-2 pr-2 py-0.5 flex flex-row rounded-l-full gap-2 items-center border-r border-r-zinc-700 hover:bg-zinc-700 transition-all">
 					<GitHubLogoIcon className="w-4 h-4" />
 					<span className="text-sm font-medium">Sign in</span>
