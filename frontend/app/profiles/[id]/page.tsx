0f7f8c722e0298ea2bcfc5cfb65aad477883d634
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-09-13 23:02:51 -0400

    Add NotFound for Profile

--- frontend/app/profiles/[id]/page.tsx
+++ frontend/app/profiles/[id]/page.tsx
@@ -1,7 +1,9 @@
 import Json from "@/components/debugging/Json"
 import Avatar from "@/components/elements/Avatar"
 
-import profileController from "@/controllers/profile"
+import api from "@/api"
+import Profile from "@/models/Profile"
+import { Link } from "@/components/elements"
 
 interface Props {
   params: {
@@ -9,10 +11,10 @@ interface Props {
   }
 }
 
-export default async function Profile({ params }: Props) {
-  const profile = await profileController.get.byId(params.id)
+export default async function ProfilePage({ params }: Props) {
+  const profile = await api.get<Profile>(`/profiles/${params.id}`)
 
-  if (!profile) return <div>Profile not found</div>
+  if (!profile) return <NotFound />
 
   return (
     <main className="mx-auto my-8 flex flex-col items-center px-2 w-full max-w-6xl gap-4 sm:gap-2">
@@ -21,3 +23,14 @@ export default async function Profile({ params }: Props) {
     </main>
   )
 }
+
+const NotFound = () => (
+  <main className="mx-auto my-8 flex flex-col items-center px-2 py-24 w-full max-w-6xl gap-4 sm:gap-2">
+    <div className="flex flex-col items-center">
+      <p>Profile not found.</p>
+      <Link href="/" variant={{ underline: true }}>
+        Return home
+      </Link>
+    </div>
+  </main>
+)
