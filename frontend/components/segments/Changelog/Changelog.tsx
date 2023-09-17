d3891dab0bbf9f0092c8278bbc352118134eb522
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-09-16 21:15:49 -0400

    Improve the look of the Changelog in the Shoulder

--- frontend/components/segments/Changelog/Changelog.tsx
+++ frontend/components/segments/Changelog/Changelog.tsx
@@ -9,7 +9,10 @@ interface Props {
 export default function Changelog({ changelog }: Props) {
 	const tokens = marked.lexer(changelog)
 
-	return <div>{tokens.map(formatToken)}</div>
+	return <section className="flex flex-col">
+		<h1 className="mb-1">Changelog</h1>
+		{tokens.map(formatToken)}
+	</section>
 }
 
 // This heavily relies on the structure of the changelog.
@@ -19,20 +22,20 @@ const formatToken = (token: any, _: number): Option<React.ReactNode> => {
 			switch (token.depth) {
 				case 2:
 					return (
-						<div className="pl-2 border-l-zinc-500 border-l">
-							<h2 className="mb-1 flex flex-col text-sm">
-								<span className="text-zinc-500">
-									{token.text.split(" ")[0]}
-								</span>
-								<span className="font-medium text-zinc-300 transition-all hover:underline">
-									<Link href="/changelog">
+						<div className="hover:pl-2 border-l-zinc-700 transition-all hover:border-l">
+							<Link href="/changelog">
+								<h2 className="mb-1 flex flex-col">
+									<span className="text-zinc-500 text-xs font-mono">
+										{token.text.split(" ")[0].replaceAll("`", "")}
+									</span>
+									<span className="font-medium text-zinc-300 text-sm">
 										{token.text
 											.split(" ")
 											.slice(1)
 											.join(" ")}
-									</Link>
-								</span>
-							</h2>
+									</span>
+								</h2>
+							</Link>
 						</div>
 					)
 				default:
