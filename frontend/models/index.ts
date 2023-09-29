eb4f9d08c79cb96901c5e2b4985e3f195fa23a3c
Thomas Eckert <teckert@hashicorp.com>
2023-09-29 10:42:18 -0700

    Add a barrel file for models

---
+++ frontend/models/index.ts
@@ -0,0 +1,11 @@
+import Blog from "./Blog"
+import Bookmark from "./Bookmark"
+import Feed from "./Feed"
+import Like from "./Like"
+import Post from "./Post"
+import Profile from "./Profile"
+import Repo from "./Repo"
+import Upload from "./Upload"
+import User from "./User"
+
+export type { Blog, Bookmark, Feed, Like, Post, Profile, Repo, Upload, User }
