7e457fc03146f362eec535014eec67fa5be681d2
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-10-02 19:30:03 -0400

    Add a "loading" component

---
+++ frontend/app/loading/page.tsx
@@ -0,0 +1,9 @@
+import Loading from "@/lib/loading/Loading"
+
+export default async function LoadingPage() {
+  return (
+    <section className="w-full h-96 flex items-center justify-center">
+      <Loading />
+    </section>
+  )
+}
