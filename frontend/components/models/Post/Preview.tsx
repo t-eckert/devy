7bd2148dafd17637f2b49dea637d1795aa269f39
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-10-02 21:11:42 -0400

    Add likes to post preview

--- frontend/components/models/Post/Preview.tsx
+++ frontend/components/models/Post/Preview.tsx
@@ -1,41 +1,50 @@
 "use client"
 
+import { HeartIcon, HeartFilledIcon } from "@radix-ui/react-icons"
+
 import Post from "@/models/Post"
 import Date from "@/components/elements/Date"
-import { Link } from "@/components/elements"
+import { Button, Link } from "@/components/elements"
 
 export default function Preview(post: Post) {
+	const likedByUser = Math.random() > 0.5
+
+
 	return (
-		<div className="flex flex-col gap-2">
+		<section className="flex flex-row gap-2 items-start">
+			<div className="flex flex-col items-start gap-1">
+				<Button className="flex flex-row gap-1 items-center w-14 px-2 py-0.5 rounded-md">
+					{likedByUser ?
+						<HeartFilledIcon className="text-zinc-300 h-3 aspect-square" /> :
+						<HeartIcon className="text-zinc-300 h-3 aspect-square" />
+					}
+					<span className="text-sm">{Math.round(Math.random() * 30)}</span>
+				</Button>
+			</div>
+
 			<div className="flex flex-row items-start gap-1">
 				<div className="flex flex-col gap-1">
 					<Link
 						href={`${post.blogSlug}/${post.slug}`}
-						variant={{ underline: false }}
-						className="font-medium"
+						className="font-medium text-zinc-50"
 					>
-						<h2>{post.title}</h2>
+						{post.title}
 					</Link>
+
 					<div className="mb-2 flex flex-row gap-2 items-baseline text-sm">
-						<div className="bg-zinc-800 text-xs flex items-center justify-center px-2 py-1 rounded-full">
-							<Date date={post.createdAt} />
-						</div>
 						<Link
 							href={`/profiles/${post.authorSlug}`}
-							variant={{ underline: false }}
 						>
 							{post.authorName}
 						</Link>
-						<span>/</span>
 						<Link
 							href={`/${post.blogSlug}`}
-							variant={{ underline: false }}
 						>
 							{post.blogName}
 						</Link>
 					</div>
 				</div>
 			</div>
-		</div>
+		</section>
 	)
 }
