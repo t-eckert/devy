909a30531fd9590c0ad5bb5fedfc09a08f654ee5
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-10-13 20:16:27 -0400

    A user can like a post

--- frontend/components/models/Post/Preview.tsx
+++ frontend/components/models/Post/Preview.tsx
@@ -5,13 +5,24 @@ import { HeartIcon, HeartFilledIcon } from "@radix-ui/react-icons"
 import Post from "@/models/Post"
 import { Button, Link } from "@/components/elements"
 
+import api from "@/lib/api"
+
 export default function Preview(post: Post) {
 	const likedByUser = false
+	const user = "e2f0fa7e-4517-4ac8-bbc6-73067d3feed4"
+
+	const like = async () => {
+		console.log("like")
+		await api.post(`/posts/${post.id}/likes`, 10, user)
+	}
 
 	return (
 		<section className="flex flex-row gap-2 items-start">
 			<div className="flex flex-col items-start gap-1">
-				<Button className="flex flex-row gap-1 items-center w-14 px-2 py-0.5 rounded-md">
+				<Button
+					className="flex flex-row gap-1 items-center w-14 px-2 py-0.5 rounded-md"
+					onClick={like}
+				>
 					{likedByUser ? (
 						<HeartFilledIcon className="text-zinc-300 h-3 aspect-square" />
 					) : (
