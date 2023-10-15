ccd4dfe0b4765ba38d1358402d1b5915ce71be23
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-10-14 22:02:24 -0400

    Add 404 and 500 illustrations

---
+++ frontend/app/four/page.tsx
@@ -0,0 +1,9 @@
+import Component404 from "@/components/404"
+
+export default async function Page() {
+  return (
+    <div className="mx-auto my-[20vh] flex flex-col px-2 w-full max-w-6xl items-center justify-center">
+      <Component404 />
+    </div>
+  )
+}
---
+++ frontend/components/Grid/Grid.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import { useEffect, useRef } from "react"
+
+import Cell from "./Cell"
+
+interface Props {
+	height: number
+	width: number
+	cells: Cell[]
+}
+
+// Cell dimensions in pixels
+const cellWidth = 8
+const cellHeight = 16
+
+const gap = 2
+
+export default function Grid({ height, width, cells }: Props) {
+	const canvasRef = useRef<HTMLCanvasElement>(
+		document.createElement("canvas")
+	)
+
+	useEffect(() => {
+		const canvas = canvasRef.current
+		if (!canvas) return
+
+		const ctx = canvas.getContext("2d")
+		if (!ctx) return
+
+		for (const cell of cells) {
+			ctx.fillStyle = cell.fill
+
+			const { x, y } = cell
+
+			const col = x * (cellWidth + gap)
+			const row = y * (cellHeight + gap)
+
+			ctx.fillRect(col, row, cellWidth, cellHeight)
+		}
+	}, [canvasRef, cells, height, width])
+
+	return (
+		<div>
+			<canvas
+				ref={canvasRef}
+				width={`${width * cellWidth + gap * (width - 1)}px`}
+				height={`${height * cellHeight + gap * (height - 1)}px`}
+			/>
+		</div>
+	)
+}
