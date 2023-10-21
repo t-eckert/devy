d093c53cb43187dfae7c52c7a1aa27cb1a64eba9
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-10-20 22:51:13 -0400

    Containerize the backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,7 @@
-use axum::{
-    routing::{delete, get, post},
-    Router,
-};
-use shuttle_secrets::SecretStore;
 use sqlx::postgres::PgPoolOptions;
+use std::env;
+use std::net::SocketAddr;
 use std::time::Duration;
-use tower_http::cors::{Any, CorsLayer};
 
 mod api;
 mod auth;
@@ -13,34 +9,38 @@ mod entities;
 mod router;
 mod store;
 
-#[shuttle_runtime::main]
-async fn axum(#[shuttle_secrets::Secrets] secret_store: SecretStore) -> shuttle_axum::ShuttleAxum {
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().expect("Failed to load .env file");
+
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
     // Connect to the database.
-    let db_connection_str = secret_store
-        .get("DATABASE_URL")
-        .expect("DATABASE_URL environment variable not set");
+    let db_connection_str = env::var("DATABASE_URL").expect("DATABASE_URL not set");
     let pool = PgPoolOptions::new()
         .max_connections(5)
         .acquire_timeout(Duration::from_secs(3))
         .connect(&db_connection_str)
         .await
         .expect("Failed to connect to database");
+    sqlx::migrate!().run(&pool).await.unwrap();
 
     // Create the auth client
-    let client_id = secret_store
-        .get("CLIENT_ID")
-        .expect("CLIENT_ID environment variable not set");
-    let client_secret = secret_store
-        .get("CLIENT_SECRET")
-        .expect("CLIENT_SECRET environment variable not set");
-    let post_auth_redirect_uri = secret_store
-        .get("POST_AUTH_REDIRECT_URI")
-        .expect("POST_AUTH_REDIRECT_URI environment variable not set");
+    let client_id = env::var("CLIENT_ID").expect("CLIENT_ID not set");
+    let client_secret = env::var("CLIENT_SECRET").expect("CLIENT_SECRET not set");
+    let post_auth_redirect_uri = env::var("POST_AUTH_URI").expect("POST_AUTH_URI not set");
     let auth_client = auth::Client::new(client_id, client_secret, post_auth_redirect_uri);
 
     let store = store::Store::new(pool, auth_client);
 
     let router = router::make_router(store);
 
-    Ok(router.into())
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(router.into_make_service())
+        .await
+        .unwrap();
 }
