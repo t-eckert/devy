bb6daea9fc5b8a74225c30cfba9cf157c3cb2247
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-10-29 16:27:44 -0400

    Add custom colors to Tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,23 @@ module.exports = {
     "./lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      "neutral-dark": "#09090B",
+      "neutral-low": "#18181B",
+      "neutral-medium": "#27272A",
+      "neutral-high": "#71717A",
+      "neutral-light": "#FAFAFA",
+      "red-primary": "#DC2626",
+      "red-high": "#FECACA",
+      "amber-primary": "#CA8A04",
+      "amber-high": "#FEF9C3",
+      "green-primary": "#16A34A",
+      "green-high": "#DCFCE7",
+      "purple-primary": "#7C3AED",
+      "purple-high": "#EDE9FE",
+      "blue-primary": "#0284C7",
+      "blue-high": "#E0F2FE",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
