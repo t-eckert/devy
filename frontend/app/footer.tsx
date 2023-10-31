d113a652841653a6d8bdf14b3b9c5020ca187b6d
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-10-30 21:44:23 -0400

    Separate out header and footer

---
+++ frontend/app/footer.tsx
@@ -0,0 +1,48 @@
+import Link from "@/components/link"
+import Logomark from "@/components/logomark"
+
+interface Props {
+  version: string
+}
+
+export default function Footer({ version }: Props) {
+  return (
+    <footer className="w-full dark:bg-neutral-dark">
+      <div className="mx-auto max-w-6xl px-2 pt-8 pb-44">
+        <div className="w-56 pb-2 border-b border-b-zinc-700 flex flex-row gap-2 items-baseline">
+          <Link href="/">
+            <Logomark />
+          </Link>
+          <span className="text-xs rounded-full px-2 py-0.5 select-none bg-zinc-800">
+            Pre-release {version}
+          </span>
+        </div>
+
+        <div className="pt-2 flex flex-row gap-12 text-sm text-zinc-300">
+          <section className="w-56 flex flex-col gap-4">
+            <p>
+              Devy is an open source platform for people to publish blog posts
+              in markdown from their GitHub repositories.{" "}
+            </p>
+            <p>Thank you for checking it out.</p>
+            <span className="self-end">
+              &#8212; <Link href="https://thomaseckert.dev">Thomas Eckert</Link>
+            </span>
+          </section>
+
+          <section className="flex flex-col gap-1 items-start">
+            <Link href="/changelog" className="font-medium">
+              Changelog
+            </Link>
+            <Link
+              href="https://github.com/t-eckert/devy"
+              className="font-medium"
+            >
+              Repo
+            </Link>
+          </section>
+        </div>
+      </div>
+    </footer>
+  )
+}
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,9 +1,10 @@
-import Nav from "./nav"
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import Link from "@/components/link"
 import { CaptureSession } from "@/lib/auth"
+
+import Header from "./header"
+import Footer from "./footer"
 
 const version = "v0.2.0"
 
@@ -24,63 +25,18 @@ export default function RootLayout({ children }: Props) {
       <body
         className={[
           inter.className,
-          "bg-zinc-50 text-zinc-950 dark:bg-zinc-900 dark:text-zinc-50",
+          "bg-neutral-light text-neutral-low dark:bg-neutral-low dark:text-neutral-light",
         ].join(" ")}
       >
         <CaptureSession />
 
         <div className="min-h-screen">
-          <header className="mx-auto max-w-6xl px-2 py-3 flex flex-row justify-between items-center">
-            <div className="flex flex-row gap-2 items-baseline">
-              <Link href="/" className="font-semibold">
-                Devy
-              </Link>
-            </div>
-
-            <Nav />
-          </header>
+          <Header />
 
           <main>{children}</main>
         </div>
 
-        <footer className="w-full bg-zinc-950">
-          <div className="mx-auto max-w-6xl px-2 pt-8 pb-44">
-            <div className="w-56 pb-2 border-b border-b-zinc-700 flex flex-row gap-2 items-baseline">
-              <Link href="/">
-                <h1 className="font-semibold">Devy</h1>
-              </Link>
-              <span className="text-xs rounded-full px-2 py-0.5 select-none bg-zinc-800">
-                Pre-release {version}
-              </span>
-            </div>
-
-            <div className="pt-2 flex flex-row gap-12 text-sm text-zinc-300">
-              <section className="w-56 flex flex-col gap-4">
-                <p>
-                  Devy is an open source platform for people to publish blog
-                  posts in markdown from their GitHub repositories.{" "}
-                </p>
-                <p>Thank you for checking it out.</p>
-                <span className="self-end">
-                  &#8212;{" "}
-                  <Link href="https://thomaseckert.dev">Thomas Eckert</Link>
-                </span>
-              </section>
-
-              <section className="flex flex-col gap-1 items-start">
-                <Link href="/changelog" className="font-medium">
-                  Changelog
-                </Link>
-                <Link
-                  href="https://github.com/t-eckert/devy"
-                  className="font-medium"
-                >
-                  Repo
-                </Link>
-              </section>
-            </div>
-          </div>
-        </footer>
+        <Footer version={version} />
       </body>
     </html>
   )
