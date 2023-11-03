bd75028bd047ed9bab6d3603a2ec3f165d022188
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-11-02 22:15:48 -0400

    Add next and previous buttons to post-collection

--- frontend/components/post-collection/post-collection.tsx
+++ frontend/components/post-collection/post-collection.tsx
@@ -1,5 +1,7 @@
-import { Post } from "@/models"
 import PostPreview from "@/components/post-preview"
+import Button from "@/components/button"
+
+import { Post } from "@/models"
 
 interface Props {
 	posts: Post[]
@@ -9,11 +11,14 @@ interface Props {
 
 export default function PostCollection({ posts, next, prev }: Props) {
 	return (
-		<section>
+		<section className="w-full max-w-lg flex flex-col gap-4">
 			{posts.map((post, index) => (
 				<PostPreview key={index} post={post} />
 			))}
+			<div className="border-t border-t-neutral-medium py-4 w-full flex flex-row-reverse justify-between">
+				{next && <Button onClick={next}>Next</Button>}
+				{prev && <Button onClick={prev}>Previous</Button>}
+			</div>
 		</section>
 	)
 }
-
