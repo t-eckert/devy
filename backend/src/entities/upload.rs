080d0d558a7730445d9f230a4dbbbc5ae3c720fb
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-11-05 09:20:20 -0500

    Add the Upload entity

--- backend/src/entities/upload.rs
+++ backend/src/entities/upload.rs
@@ -3,5 +3,14 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Upload {
-    pub id: String,
+    pub id: Option<String>,
+
+    pub previous_upload_id: Option<String>,
+
+    pub created_at: Option<String>,
+    pub updated_at: Option<String>,
+
+    pub status: Option<String>,
+    pub repo: Option<String>,
+    pub logs: Option<Vec<String>>,
 }
