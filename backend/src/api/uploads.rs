ef35a0cd431c92f8bd3f48f59ea33552e46fd877
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-11-05 10:08:23 -0500

    Insert upload into database when posted

--- backend/src/api/uploads.rs
+++ backend/src/api/uploads.rs
@@ -1,7 +1,24 @@
-use crate::entities::Upload;
-use axum::{http::StatusCode, Json};
+use crate::{entities::Upload, store::Store};
+use axum::{
+    extract::{Json as ExtractJson, State},
+    http::StatusCode,
+    Json,
+};
 
-pub async fn post_upload() -> Result<Json<Upload>, StatusCode> {
-    println!("Heyo!");
-    Err(StatusCode::ACCEPTED)
+pub async fn post_upload(
+    State(store): State<Store>,
+    ExtractJson(payload): ExtractJson<Upload>,
+) -> Result<Json<Upload>, StatusCode> {
+    println!("Welcome to the uploads route!");
+
+    let upload = match payload.insert(&store.pool).await {
+        Ok(upload) => upload,
+        Err(_) => return Err(StatusCode::INTERNAL_SERVER_ERROR),
+    };
+
+    /* x Insert the upload to the database.
+     * Once inserted, pass the upload to the uploader.
+     */
+
+    Ok(Json(upload))
 }
