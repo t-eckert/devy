9003ec8151f4359df089c7b3f7f923c8ea540c48
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-11-11 22:30:28 -0500

    Add auth::error

---
+++ backend/src/auth/error.rs
@@ -0,0 +1,24 @@
+use serde::Serialize;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+/// Errors that can occur during authentication processes.
+#[derive(Debug, Serialize)]
+pub enum Error {
+    /// GitHub returned an error when exchanging the code for a token.
+    CodeExchangeForTokenFailed(String),
+
+    /// GitHub returned an error when exchanging the token for a user.
+    TokenExchangeForUserFailed(String),
+
+    /// The GitHub user data could not be deserialized.
+    UnableToDeserializeUser(String),
+}
+
+impl core::fmt::Display for Error {
+    fn fmt(&self, fmt: &mut core::fmt::Formatter) -> core::result::Result<(), core::fmt::Error> {
+        write!(fmt, "{self:?}")
+    }
+}
+
+impl std::error::Error for Error {}
