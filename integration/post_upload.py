3fb46bfabefb00ecdf74c635204e5cda476d3e8d
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-11-15 22:33:10 -0500

    Clone devy-test-repo

--- integration/post_upload.py
+++ integration/post_upload.py
@@ -4,7 +4,7 @@ import httpx
 import json
 
 upload = {
-    "repo": "https://github.com/t-eckert/devy"
+    "repo": "https://github.com/t-eckert/devy-test-repo"
 }
 
 r = httpx.post(f"{url}uploads", json=upload)
