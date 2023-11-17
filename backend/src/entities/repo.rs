824e3a320edc9ead81d970fccdcaf5fe4f500de5
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-11-16 22:49:57 -0500

    Posts get inserted into the database

--- backend/src/entities/repo.rs
+++ backend/src/entities/repo.rs
@@ -1,17 +1,37 @@
+use super::error::Result;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use sqlx::PgPool;
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Repo {
-    id: Option<String>,
-    url: Option<String>,
-    name: Option<String>,
+    pub id: Option<String>,
+    pub blog_id: Option<String>,
 
-    github_id: Option<i32>,
-    github_name: Option<String>,
-    metadata: Option<String>,
+    pub url: Option<String>,
+    pub github_id: Option<i64>,
+    pub github_name: Option<String>,
+    pub metadata: Option<Value>,
 
-    created_at: Option<String>,
-    updated_at: Option<String>,
+    pub created_at: Option<String>,
+    pub updated_at: Option<String>,
 }
 
-impl Repo {}
+impl Repo {
+    pub async fn get_by_url(pool: &PgPool, url: &str) -> Result<Self> {
+        Ok(sqlx::query_as!(
+            Self,
+            r#"
+                SELECT 
+                    id::TEXT, blog_id::TEXT, url, github_id, github_name, metadata,
+                    to_char(repo.created_at, 'YYYY-MM-DDThh:mm:ss.ss') AS created_at,
+                    to_char(repo.updated_at, 'YYYY-MM-DDThh:mm:ss.ss') AS updated_at
+                FROM "repo" WHERE url = $1;
+                "#,
+            url
+        )
+        .fetch_one(pool)
+        .await?)
+    }
+}
