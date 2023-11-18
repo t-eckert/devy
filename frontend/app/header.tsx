13277712baa76c3ec65e6501046ab8386e3843d3
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-11-17 23:39:25 -0500

    Add a border to the bottom of the header

--- frontend/app/header.tsx
+++ frontend/app/header.tsx
@@ -5,17 +5,19 @@ import Nav from "./nav"
 
 export default function Header() {
   return (
-    <header className="mx-auto max-w-6xl px-2 py-3 flex flex-row justify-between items-center">
-      <div className="flex flex-row gap-2 items-baseline">
-        <Link href="/" variant={{ underline: false }}>
-          <Logomark />
-        </Link>
-        <span className="text-xs font-medium px-2 py-0.5 rounded-full bg-neutral-medium select-none">
-          Preview
-        </span>
-      </div>
+    <header className="border-b border-b-neutral-1">
+      <div className="mx-auto max-w-6xl px-2 py-3 flex flex-row justify-between items-center">
+        <div className="flex flex-row gap-2 items-baseline">
+          <Link href="/" variant={{ underline: false }}>
+            <Logomark />
+          </Link>
+          <span className="text-xs font-medium px-2 py-0.5 rounded-full bg-neutral-medium select-none">
+            Preview
+          </span>
+        </div>
 
-      <Nav />
+        <Nav />
+      </div>
     </header>
   )
 }
