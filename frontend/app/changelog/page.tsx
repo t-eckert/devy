1ea699ff3943b21baa4942d2e8a1054ad0bd38f8
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-11-20 21:38:42 -0500

    Inline the request to get the Changelog

--- frontend/app/changelog/page.tsx
+++ frontend/app/changelog/page.tsx
@@ -1,11 +1,10 @@
 import Markdown from "@/components/markdown"
 
-import { fetchChangelog } from "@/lib/changelog"
-
-export default async function Changelog() {
-  const changelog = await fetchChangelog()
-
-  if (!changelog) return <div>Changelog not found</div>
+export default async function ChangelogPage() {
+  const res = await fetch(
+    "https://raw.githubusercontent.com/t-eckert/devy/main/CHANGELOG.md"
+  )
+  const changelog = await res.text()
 
   return (
     <main className="mx-auto my-4 flex flex-col sm:flex-row px-2 w-full max-w-6xl gap-4 sm:gap-2">
--- frontend/lib/changelog.ts
+++
@@ -1,6 +0,0 @@
-const url = "https://raw.githubusercontent.com/t-eckert/devy/main/CHANGELOG.md"
-
-export const fetchChangelog = async (): Promise<string> => {
-	const res = await fetch(url)
-	return await res.text()
-}
