6fe0d07eb21cce839044bca33435f4b0bf167b81
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-11-20 21:33:08 -0500

    Clean up the post preview component

--- frontend/components/post-preview/post-preview.tsx
+++ frontend/components/post-preview/post-preview.tsx
@@ -2,7 +2,6 @@ import Post from "@/models/Post"
 import Link from "@/components/link"
 import Likes from "@/components/likes"
 
-import Title from "./post-title"
 
 interface Props {
 	post: Post
@@ -11,29 +10,40 @@ interface Props {
 export default function PostPreview({ post }: Props) {
 	return (
 		<section className={wrapper}>
-			<div className="flex flex-col items-start gap-1">
-				<Likes postId={post.id} initialCount={post.likes} />
-			</div>
-
-			<div className="flex flex-col gap-1">
-				<Title
-					title={post.title}
-					url={`${post.blogSlug}/${post.slug}`}
-				/>
-
-				<div className="flex flex-row gap-2 items-baseline text-xs">
+			<div className="flex flex-col">
+				<div className="ml-14 text-xs">
 					<Link
-						href={`/profiles/${post.authorSlug}`}
+						href={{ pathname: `/${post.blogSlug}` }}
 						variant={{ underline: false }}
-					>
-						{post.authorName}
-					</Link>
-					<Link
-						href={`/${post.blogSlug}`}
-						variant={{ underline: false }}
+						className="text-neutral hover:text-neutral-1 hover:dark:text-neutral+1"
 					>
 						{post.blogName}
 					</Link>
+				</div>
+
+				<div className="flex flex-row gap-2">
+					<div className="w-12 flex flex-col items-start gap-1">
+						<Likes postId={post.id} initialCount={post.likes} />
+					</div>
+
+					<div className="flex flex-col">
+						<Link
+							href={{ pathname: `/${post.blogSlug}/${post.slug}` }}
+							className="font-medium text-zinc-50"
+							variant={{ underline: false }}
+						>
+							{post.title}
+						</Link>
+
+						<div className="flex flex-row gap-2 items-baseline text-xs">
+							<Link
+								href={{ pathname: `/profiles/${post.authorSlug}` }}
+								variant={{ underline: false }}
+							>
+								{post.authorName}
+							</Link>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
@@ -41,14 +51,9 @@ export default function PostPreview({ post }: Props) {
 }
 
 const wrapper = [
-	"flex",
-	"flex-row",
-
-	"gap-4",
-	"items-start",
-
 	"py-2",
 	"px-4",
+	"w-full",
 
 	"dark:border-neutral-1",
 
