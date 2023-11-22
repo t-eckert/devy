a9af8c1a3cafcf540d2c54467011ddbf0c75a381
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-11-21 23:32:04 -0500

    Add the header to the Changelog page

--- frontend/app/changelog/page.tsx
+++ frontend/app/changelog/page.tsx
@@ -12,10 +12,11 @@ export default async function ChangelogPage() {
   const changelog = await res.text()
 
   return (
-    <main className="mx-auto my-4 flex flex-col sm:flex-row px-2 w-full max-w-6xl gap-4 sm:gap-2">
-      <article className="mx-auto w-full max-w-xl">
-        <Markdown content={changelog} />
-      </article>
+    <main className="mx-auto my-4 flex flex-col px-2 py-12 w-full max-w-2xl gap-4 sm:gap-2">
+      <h1 className="text-6xl mb-2 font-semibold text-neutral-1 dark:text-neutral+1">
+        Changelog
+      </h1>
+      <Markdown content={changelog} />
     </main>
   )
 }
