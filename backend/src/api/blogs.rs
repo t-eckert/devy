adcbd5efccd1c97ce383bfd3ddba61c840c04c51
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-11-23 21:19:06 -0500

    Refactor the api functions

--- backend/src/api/blogs.rs
+++ backend/src/api/blogs.rs
@@ -1,74 +1,34 @@
-use crate::{
-    entities::{error::Error, Blog, Post},
-    store::Store,
-};
+use super::error::Result;
+use crate::{entities::Blog, store::Store};
 use axum::{
     extract::{Path, State},
     http::StatusCode,
     Json,
 };
 
-/// Get a blog by its slug.
-/// `/blogs/:blog_slug`
-pub async fn get_blog_by_blog_slug(
+pub async fn get_by_username(
+    State(store): State<Store>,
+    Path(username): Path<String>,
+) -> Result<Json<Vec<Blog>>> {
+    Ok(Json(Blog::get_by_username(&store.pool, username).await?))
+}
+
+pub async fn get_by_blog_slug(
     State(store): State<Store>,
     Path(blog_slug): Path<String>,
-) -> Result<Json<Blog>, StatusCode> {
-    Ok(Json(
-        Blog::get_by_slug(&store.pool, blog_slug)
-            .await
-            .map_err(map_error)?,
-    ))
+) -> Result<Json<Blog>> {
+    Ok(Json(Blog::get_by_slug(&store.pool, blog_slug).await?))
 }
 
-/// Get all posts by a blog slug.
-/// `/blogs/:blog_slug/posts`
-pub async fn get_blog_posts_by_blog_slug(
+pub async fn upsert(State(store): State<Store>, Json(blog): Json<Blog>) -> Result<Json<Blog>> {
+    Ok(Json(blog.upsert(&store.pool).await?))
+}
+
+pub async fn delete(
     State(store): State<Store>,
     Path(blog_slug): Path<String>,
-) -> Result<Json<Vec<Post>>, StatusCode> {
-    Ok(Json(
-        Post::get_by_blog_slug(&store.pool, blog_slug)
-            .await
-            .map_err(map_error)?,
-    ))
-}
-
-/// Get a post by its blog slug and post slug.
-/// `/blogs/:blog_slug/posts/:post_slug`
-pub async fn get_post_by_blog_and_post_slug(
-    State(store): State<Store>,
-    Path((blog_slug, post_slug)): Path<(String, String)>,
-) -> Result<Json<Post>, StatusCode> {
-    Ok(Json(
-        Post::get_by_blog_and_post_slug(&store.pool, blog_slug, post_slug)
-            .await
-            .map_err(map_error)?,
-    ))
-}
-
-pub async fn upsert_blog(
-    State(store): State<Store>,
-    Json(blog): Json<Blog>,
-) -> Result<Json<Blog>, StatusCode> {
-    Ok(Json(blog.upsert(&store.pool).await.map_err(map_error)?))
-}
-
-pub async fn delete_blog(
-    State(store): State<Store>,
-    Path(blog_slug): Path<String>,
-) -> Result<StatusCode, StatusCode> {
-    Blog::delete_by_slug(&store.pool, blog_slug)
-        .await
-        .map_err(map_error)?;
+) -> Result<StatusCode> {
+    Blog::delete_by_slug(&store.pool, blog_slug).await?;
 
     Ok(StatusCode::OK)
-}
-
-fn map_error(e: Error) -> StatusCode {
-    match e {
-        Error::EntityNotFound => StatusCode::NOT_FOUND,
-        Error::Malformed { .. } => StatusCode::BAD_REQUEST,
-        Error::Sqlx(_) => StatusCode::INTERNAL_SERVER_ERROR,
-    }
 }
--- backend/src/api/likes.rs
+++ backend/src/api/likes.rs
@@ -1,29 +1,30 @@
+use super::error::Result;
 use crate::{entities::Like, store::Store};
 use axum::{
     extract::{Json as ExtractJson, Path, State},
-    http::StatusCode,
     Json,
 };
+
+pub async fn get_ids_by_username(
+    State(store): State<Store>,
+    Path(username): Path<String>,
+) -> Result<Json<Vec<String>>> {
+    Ok(Json(
+        Like::get_post_ids_by_username(&store.pool, username).await?,
+    ))
+}
 
 pub async fn post_like(
     State(store): State<Store>,
     ExtractJson(like): ExtractJson<Like>,
-) -> Result<Json<Like>, StatusCode> {
-    Ok(Json(
-        like.upsert(&store.pool)
-            .await
-            .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?,
-    ))
+) -> Result<Json<Like>> {
+    Ok(Json(like.upsert(&store.pool).await?))
 }
 
 pub async fn delete_like(
     State(store): State<Store>,
     Path((post_id, profile_id)): Path<(String, String)>,
-) -> Result<Json<Like>, StatusCode> {
+) -> Result<Json<Like>> {
     let like = Like::new(profile_id, post_id);
-    Ok(Json(
-        like.delete(&store.pool)
-            .await
-            .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?,
-    ))
+    Ok(Json(like.delete(&store.pool).await?))
 }
