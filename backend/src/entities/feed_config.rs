76fffa8dbf6cd9391251acc515f6d96b7d54733c
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-11-24 16:44:46 -0500

    Add the FeedConfigRepository and endpoint

--- backend/src/entities/feed_config.rs
+++ backend/src/entities/feed_config.rs
@@ -1,8 +1,32 @@
+use super::error::Result;
+
 use serde::{Deserialize, Serialize};
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct FeedConfig {
-    pub id: String,
-    pub name: String,
+    pub id: Uuid,
+    pub name: Option<String>,
+}
+
+pub struct FeedConfigRepository {}
+
+impl FeedConfigRepository {
+    pub async fn get_by_id(pool: &PgPool, id: Uuid) -> Result<FeedConfig> {
+        Ok(sqlx::query_as!(
+            FeedConfig,
+            r#"
+            SELECT 
+                id,
+                name
+            FROM "feed_config"
+            WHERE id = $1;
+            "#,
+            id
+        )
+        .fetch_one(pool)
+        .await?)
+    }
 }
