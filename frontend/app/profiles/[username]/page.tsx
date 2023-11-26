a04fe8ffa08021f0abf54d38003c1c4788b39a0a
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-11-26 18:31:39 -0500

    Create the new Member Card

--- frontend/app/profiles/[username]/page.tsx
+++ frontend/app/profiles/[username]/page.tsx
@@ -1,11 +1,10 @@
 import api from "@/lib/api"
 import Profile from "@/models/Profile"
 import User from "@/models/User"
-import Member from "@/components/member"
 import Blog from "@/models/Blog"
 import Post from "@/models/Post"
 import PostCollection from "@/components/post-collection"
-import Card from "@/components/card"
+import MemberCard from "@/components/member-card"
 import Markdown from "@/components/markdown"
 
 interface Props {
@@ -31,10 +30,8 @@ export default async function ProfilePage({ params }: Props) {
   )
 
   return (
-    <main className="mx-auto my-8 flex flex-row items-start px-2 w-full max-w-6xl gap-4 sm:gap-16">
-      <div>
-        <Member profile={profile} user={user} blogs={blogs} />
-      </div>
+    <main className="mx-auto my-8 flex flex-col items-center sm:flex-row sm:items-start px-2 w-full max-w-6xl gap-4 sm:gap-16">
+      <MemberCard profile={profile} user={user} blogs={blogs} />
 
       <div>
         <div className="flex flex-col gap-2 w-full max-w-3xl">
