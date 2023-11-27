e73565a89f72e6b4ce7f40208c688b62c6a88f4b
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-11-26 22:40:59 -0500

    Update version in the packages to 0.3.4

--- frontend/config.ts
+++ frontend/config.ts
@@ -5,7 +5,7 @@ export interface Config {
 }
 
 const config: Config = {
-	VERSION: "0.3.4",
+	VERSION: "0.3.5",
 	HOST: process.env.HOST ?? "http://localhost:3000",
 	API: process.env.API ?? "http://localhost:8000",
 }
