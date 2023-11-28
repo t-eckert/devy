50aa513c4152d5018281576aab28b1125b1881d4
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-11-27 23:56:10 -0500

    Write some copy on the about page

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,7 +1,43 @@
+import Link from "@/components/link"
+
 export default function About() {
   return (
-    <main className="mx-auto my-4 flex flex-col sm:flex-row px-2 w-full max-w-6xl gap-4 sm:gap-2">
-      About
-    </main>
+    <section className="mx-auto my-4 px-2 w-full max-w-6xl items-start flex flex-col gap-4">
+      <h1 className="text-4xl font-semibold text-neutral-1 dark:text-neutral+1">
+        Markdown is the medium
+      </h1>
+
+      <p className="w-full max-w-md">
+        Devy lets you blog from a GitHub repo. Every markdown file is converted
+        to a blog post and automatically updated when you push changes.
+      </p>
+
+      <Link href="/new/blog" variant={{ underline: false }} className={cta}>
+        Create a new blog
+      </Link>
+    </section>
   )
 }
+
+const cta = [
+  "pointer-cursor",
+  "transition-all",
+  "rounded-lg",
+  "text-sm",
+  "font-medium",
+  "border",
+  "focus:outline-none",
+  "focus:ring-1",
+  "focus:ring-zinc-700",
+  "focus:ring-offset-1",
+  "dark:focus:ring-zinc-200",
+  "dark:text-zinc-50",
+  "px-2.5",
+  "py-0.5",
+  "border-neutral-darkest",
+  "bg-neutral-darker",
+  "text-neutral-lighter",
+  "dark:border-neutral-lightest",
+  "dark:bg-neutral-lightest",
+  "dark:text-neutral-darker",
+].join(" ")
