aa013c0188d78c9d8084b7b2a4c3d1c7b8a1a630
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-12-08 23:47:32 -0500

    Reduce the Changelog header size on mobile

--- frontend/app/changelog/page.tsx
+++ frontend/app/changelog/page.tsx
@@ -12,8 +12,8 @@ export default async function ChangelogPage() {
   const changelog = await res.text()
 
   return (
-    <main className="mx-auto my-4 flex flex-col px-2 py-12 w-full max-w-2xl gap-4 sm:gap-2">
-      <h1 className="text-6xl mb-2 font-semibold text-neutral-1 dark:text-neutral+1">
+    <main className="mx-auto flex flex-col px-2 py-6 sm:py-12 w-full max-w-2xl gap-4 sm:gap-2">
+      <h1 className="text-4xl sm:text-6xl sm:mb-4 font-semibold text-neutral-1 dark:text-neutral+1">
         Changelog
       </h1>
       <Markdown content={changelog} />
