d6074568243c4e414d291c35c85eb8c47c5bc9d2
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-12-10 20:59:45 -0500

    Add store for version

---
+++ frontend/lib/version.ts
@@ -0,0 +1,31 @@
+import { create } from "zustand"
+import { persist } from "zustand/middleware"
+
+export interface VersionStore {
+	currentVersion: string
+	lastVersion: string
+	setCurrentVersion: (version: string) => void
+	loadVersion: () => void
+	isNewVersion: () => boolean
+}
+
+const useVersion = create<VersionStore>()(
+	persist(
+		(set) => ({
+			currentVersion: "v0.4.0",
+			lastVersion: "v0.3.6",
+			setCurrentVersion: (version: string) => {
+				set({ currentVersion: version })
+			},
+			loadVersion: () => {
+				console.log("loadVersion")
+			},
+			isNewVersion: () => {
+				return false
+			},
+		}),
+		{ name: "version" }
+	)
+)
+
+export default useVersion
