6507629faa93f800334b22e829cf01ee41ad8da9
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-01-08 21:21:26 -0500

    Increase text width on posts

--- frontend/components/markdown/components.tsx
+++ frontend/components/markdown/components.tsx
@@ -14,32 +14,32 @@ const components = {
 		</h1>
 	),
 	h2: ({ children }: Props) => (
-		<h2 className="border-b border-b-neutral+1 dark:border-b-neutral-1 mx-auto w-full max-w-2xl mt-6 mb-4 text-2xl sm:text-4xl font-semibold">
+		<h2 className="border-b border-b-neutral+1 dark:border-b-neutral-1 mx-auto w-full max-w-3xl mt-6 mb-4 text-3xl sm:text-4xl font-semibold">
 			{children}
 		</h2>
 	),
 	h3: ({ children }: Props) => (
-		<h3 className="mx-auto w-full max-w-2xl mt-2 mb-2 text-2xl font-semibold">
+		<h3 className="mx-auto w-full max-w-3xl mt-2 mb-2 text-3xl font-semibold">
 			{children}
 		</h3>
 	),
 	h4: ({ children }: Props) => (
-		<h4 className="mx-auto w-full max-w-2xl mb-1 text-xl font-semibold">
+		<h4 className="mx-auto w-full max-w-3xl mb-1 text-xl font-semibold">
 			{children}
 		</h4>
 	),
 	h5: ({ children }: Props) => (
-		<h5 className="mx-auto w-full max-w-2xl mb-1 text-lg font-semibold">
+		<h5 className="mx-auto w-full max-w-3xl mb-1 text-lg font-semibold">
 			{children}
 		</h5>
 	),
 	h6: ({ children }: Props) => (
-		<h6 className="mx-auto w-full max-w-2xl mb-1 font-semibold">
+		<h6 className="mx-auto w-full max-w-3xl mb-1 font-semibold">
 			{children}
 		</h6>
 	),
 	p: ({ children }: Props) => (
-		<p className="mx-auto w-full max-w-2xl mb-4 leading-snug">{children}</p>
+		<p className="mx-auto w-full max-w-3xl mb-4 leading-snug">{children}</p>
 	),
 	blockquote: ({ children }: Props) => (
 		<blockquote className="border-l border-neutral+1 dark:border-neutral-1 pl-2 ">
@@ -47,7 +47,7 @@ const components = {
 		</blockquote>
 	),
 	pre: ({ children }: Props) => (
-		<pre className="mx-auto w-full max-w-2xl mb-4 mt-2 bg-neutral-3 font-mono px-2 py-1 rounded text-neutral+1 bg-zinc-800 text-sm">
+		<pre className="mx-auto w-full max-w-3xl mb-4 mt-2 bg-neutral-3 font-mono px-2 py-1 rounded text-neutral+1 bg-zinc-800 text-sm">
 			{children}
 		</pre>
 	),
@@ -57,17 +57,17 @@ const components = {
 		</code>
 	),
 	ul: ({ children }: Props) => (
-		<ul className="mx-auto w-full max-w-2xl ml-4 mb-4 list-disc list-outside">
+		<ul className="mx-auto w-full max-w-3xl ml-4 mb-4 list-disc list-outside">
 			{children}
 		</ul>
 	),
 	li: ({ children }: Props) => (
-		<li className="mb-1 leading-snug mx-auto w-full max-w-2xl">
+		<li className="mb-1 leading-snug mx-auto w-full max-w-3xl">
 			{children}
 		</li>
 	),
 	ol: ({ children }: Props) => (
-		<ol className="mx-auto w-full max-w-2xl mb-4 list-decimal list-inside">
+		<ol className="mx-auto w-full max-w-3xl mb-4 list-decimal list-inside">
 			{children}
 		</ol>
 	),
@@ -89,7 +89,7 @@ const components = {
 		<img
 			src={src}
 			alt={alt}
-			className="mx-auto my-4 max-w-2xl rounded shadow-lg bg-neutral+1 flex text-center"
+			className="mx-auto my-4 max-w-3xl rounded shadow-lg bg-neutral+1 flex text-center"
 		/>
 	),
 	a: ({ children, href }: Props) => (
