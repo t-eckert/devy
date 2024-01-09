7d3978f76b1ff1204bb00b6ed67e91af61b69b82
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-01-08 21:53:09 -0500

    Links that look like buttons!

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -7,37 +7,14 @@ export default function About() {
         Markdown is the medium
       </h1>
 
-      <p className="w-full max-w-md">
+      <p className="mb-2 w-full max-w-md">
         Devy lets you blog from a GitHub repo. Every markdown file is converted
         to a blog post and automatically updated when you push changes.
       </p>
 
-      <Link href="/new/blog" variant={{ underline: false }} className={cta}>
+      <Link href="/new/blog" asButton>
         Create a new blog
       </Link>
     </section>
   )
 }
-
-const cta = [
-  "pointer-cursor",
-  "transition-all",
-  "rounded-lg",
-  "text-sm",
-  "font-medium",
-  "border",
-  "focus:outline-none",
-  "focus:ring-1",
-  "focus:ring-zinc-700",
-  "focus:ring-offset-1",
-  "dark:focus:ring-zinc-200",
-  "dark:text-zinc-50",
-  "px-2.5",
-  "py-0.5",
-  "border-neutral-darkest",
-  "bg-neutral-darker",
-  "text-neutral-lighter",
-  "dark:border-neutral-lightest",
-  "dark:bg-neutral-lightest",
-  "dark:text-neutral-darker",
-].join(" ")
