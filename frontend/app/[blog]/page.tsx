57efa317cfde5bb9187fce6c3fff215766aa3001
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-01-15 22:46:03 -0500

    Remove the link to the author on a blog for now

--- frontend/app/[blog]/page.tsx
+++ frontend/app/[blog]/page.tsx
@@ -20,12 +20,6 @@ export default async function BlogPage({ params }: Props) {
       <main className="mx-auto my-4 flex flex-col px-2 w-full max-w-6xl gap-12">
         {blog ? (
           <header>
-            <Link
-              href={`/profiles/${blog.username}`}
-              variant={{ underline: true }}
-            >
-              {blog.profileName}
-            </Link>
             <h1 className="text-4xl font-semibold">{blog.name}</h1>
           </header>
         ) : (
