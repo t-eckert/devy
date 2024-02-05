a06ca9a1fe63a53f9d6d886f3b53eaf96740d9ea
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-02-04 21:16:44 -0500

    Stub out more components

---
+++ site/src/lib/post/preview/preview.stories.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+	import Preview from "./preview.svelte"
+
+	export const meta = {
+		title: "Post/Preview",
+		component: Preview
+	}
+</script>
+
+<script lang="ts">
+	import { Template, Story } from "@storybook/addon-svelte-csf"
+</script>
+
+<Template let:args>
+	<Preview {...args} />
+</Template>
+
+<Story name="Example"></Story>
---
+++ site/src/lib/post/preview/preview.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { Post } from "$lib/types"
+	import Json from "$lib/utils/json.svelte"
+
+	export let post: Post
+</script>
+
+<Json data={post} />
