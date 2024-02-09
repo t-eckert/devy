9531920f7e57b8da8f6d1623f08d7bcd07113ffd
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-02-08 22:57:22 -0500

    Display post on post page

---
+++ site/src/routes/[blog]/[post]/+page.server.ts
@@ -0,0 +1,12 @@
+import type { PageServerLoad } from "./$types"
+import api from "$lib/api"
+import type { Post } from "$lib/types"
+
+export const load: PageServerLoad = async ({ params }) => {
+	console.log(params)
+	return {
+		props: {
+			post: await api.get<Post>(`/v1/blogs/${params.blog}/posts/${params.post}`)
+		}
+	}
+}
--- site/src/routes/[blog]/[post]/+page.svelte
+++ site/src/routes/[blog]/[post]/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+	import type { PageData } from "./$types"
 	import Main from "$lib/layout/main.svelte"
 	import H1 from "$lib/element/h1.svelte"
+	import Json from "$lib/utils/json.svelte"
+
+	export let data: PageData
 </script>
 
 <Main>
-	<H1>Post</H1>
+	<article>{data.props.post.content}</article>
 </Main>
