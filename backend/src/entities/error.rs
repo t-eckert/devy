cbfb015775c4387b28a3851eb53bd28ddc3c9ecd
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-02-09 22:25:13 -0500

    Remove exceptions from callback

--- backend/src/entities/error.rs
+++ backend/src/entities/error.rs
@@ -4,18 +4,32 @@ use std::fmt::Debug;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
+/// Errors that can occur while performing an action on an entity.
 #[serde_as]
 #[derive(Debug, Serialize)]
 pub enum Error {
+    /// The requested entity was not found.
     EntityNotFound,
+
+    /// The request was malformed.
     Malformed(String),
+
+    /// A field was missing from the request.
     MissingField(String),
+
+    /// An error occurred while interacting with the database.
     Sqlx(#[serde_as(as = "DisplayFromStr")] sqlx::Error),
 }
 
 impl Error {
+    /// Returns an error indicating that the request was malformed.
     pub fn malformed(error: &str) -> Self {
         Self::Malformed(error.to_string())
+    }
+
+    /// Returns an error indicating that a field was missing from the request.
+    pub fn missing_field(field: &str) -> Self {
+        Self::MissingField(field.to_string())
     }
 }
 
