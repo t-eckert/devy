6f7e944c8c8bc8536eff6c1850631ea9ca253d68
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-02-15 23:36:17 -0500

    db: add post

--- crates/db/src/lib.rs
+++ crates/db/src/lib.rs
@@ -8,3 +8,4 @@ pub use error::{Error, Result};
 
 pub mod blog;
 pub mod feed_config;
+pub mod post;
--- crates/db/src/post/actions.rs
+++ crates/db/src/post/actions.rs
@@ -1,60 +1,40 @@
-use sqlx::PgPool;
+use crate::{Database, Error, Result};
+use entities::Post;
 use uuid::{uuid, Uuid};
 
-use super::*;
-
-pub async fn upsert(_pool: &PgPool, _post: PostForUpsert) -> Result<Post> {
-    // TODO
-    Ok(Post {
-        id: uuid!("5941b29d-246d-4897-a69e-3201f6ad8715"),
-        slug: "slug".to_string(),
-        blog_slug: Some("blog_slug".to_string()),
-        blog_name: None,
-        author_name: None,
-        author_username: None,
-        title: "title".to_string(),
-        content: "content".to_string(),
-        likes: None,
-        created_at: None,
-        updated_at: None,
-    })
-}
-
-pub async fn get_by_id(pool: &PgPool, id: Uuid) -> Result<Post> {
+pub async fn get_by_id(db: &Database, id: Uuid) -> Result<Post> {
     Ok(
-        sqlx::query_file_as!(Post, "src/entities/post/queries/get_by_id.sql", id)
-            .fetch_one(pool)
+        sqlx::query_file_as!(Post, "src/post/queries/get_by_id.sql", id)
+            .fetch_one(db)
             .await?,
     )
 }
 
-pub async fn get_by_blog_slug(pool: &PgPool, blog_slug: &str) -> Result<Vec<Post>> {
-    Ok(sqlx::query_file_as!(
-        Post,
-        "src/entities/post/queries/get_by_blog_slug.sql",
-        blog_slug
+pub async fn get_by_blog_slug(db: &Database, blog_slug: &str) -> Result<Vec<Post>> {
+    Ok(
+        sqlx::query_file_as!(Post, "src/post/queries/get_by_blog_slug.sql", blog_slug)
+            .fetch_all(db)
+            .await?,
     )
-    .fetch_all(pool)
-    .await?)
 }
 
 pub async fn get_by_blog_slug_and_post_slug(
-    pool: &PgPool,
+    db: &Database,
     blog_slug: &str,
     post_slug: &str,
 ) -> Result<Post> {
     Ok(sqlx::query_file_as!(
         Post,
-        "src/entities/post/queries/get_by_blog_slug_and_post_slug.sql",
+        "src/post/queries/get_by_blog_slug_and_post_slug.sql",
         blog_slug,
         post_slug
     )
-    .fetch_one(pool)
+    .fetch_one(db)
     .await?)
 }
 
 pub async fn get_by_feed(
-    pool: &PgPool,
+    db: &Database,
     feed_id: Uuid,
     limit: i64,
     offset: i64,
@@ -63,16 +43,24 @@ pub async fn get_by_feed(
     let popular = uuid!("e9173695-1b31-465f-9e79-a80224be44ad");
 
     match feed_id {
-        id if id == new => get_by_feed_new(pool, limit, offset).await,
-        id if id == popular => get_by_feed_popular(pool, limit, offset).await,
+        id if id == new => get_by_feed_new(db, limit, offset).await,
+        id if id == popular => get_by_feed_popular(db, limit, offset).await,
         _ => Err(Error::EntityNotFound),
     }
 }
 
-pub async fn get_by_feed_new(pool: &PgPool, limit: i64, offset: i64) -> Result<Vec<Post>> {
+pub async fn get_by_feed_new(db: &Database, limit: i64, offset: i64) -> Result<Vec<Post>> {
+    Ok(
+        sqlx::query_file_as!(Post, "src/post/queries/get_by_feed_new.sql", limit, offset)
+            .fetch_all(db)
+            .await?,
+    )
+}
+
+pub async fn get_by_feed_popular(pool: &Database, limit: i64, offset: i64) -> Result<Vec<Post>> {
     Ok(sqlx::query_file_as!(
         Post,
-        "src/entities/post/queries/get_by_feed_new.sql",
+        "src/post/queries/get_by_feed_popular.sql",
         limit,
         offset
     )
@@ -80,33 +68,10 @@ pub async fn get_by_feed_new(pool: &PgPool, limit: i64, offset: i64) -> Result<V
     .await?)
 }
 
-pub async fn get_by_feed_popular(pool: &PgPool, limit: i64, offset: i64) -> Result<Vec<Post>> {
-    Ok(sqlx::query_file_as!(
-        Post,
-        "src/entities/post/queries/get_by_feed_popular.sql",
-        limit,
-        offset
+pub async fn get_by_username(pool: &Database, username: &str) -> Result<Vec<Post>> {
+    Ok(
+        sqlx::query_file_as!(Post, "src/post/queries/get_by_username.sql", username)
+            .fetch_all(pool)
+            .await?,
     )
-    .fetch_all(pool)
-    .await?)
-}
-
-pub async fn get_by_username(pool: &PgPool, username: &str) -> Result<Vec<Post>> {
-    Ok(sqlx::query_file_as!(
-        Post,
-        "src/entities/post/queries/get_by_username.sql",
-        username
-    )
-    .fetch_all(pool)
-    .await?)
-}
-
-pub async fn get_liked_by_username(_pool: &PgPool, _username: &str) -> Result<Vec<Post>> {
-    return Ok(vec![]);
-
-    // Ok(
-    //     sqlx::query_file_as!(Post, "src/entities/post/queries/get_liked_by_username.sql", username)
-    //         .fetch_all(pool)
-    //         .await?,
-    // )
 }
