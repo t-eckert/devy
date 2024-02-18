126e3779838a3830f17d8b80c5619ccc79d12889
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-02-18 15:04:52 -0500

    store: add client

--- crates/store/src/lib.rs
+++ crates/store/src/lib.rs
@@ -1,8 +1,10 @@
-use database::Database;
+use db::Database;
+use auth::Client;
 
 /// Store is the shared state of the application.
 /// It contains a database pool, configuration, an authentication client, and an uploader.
 #[derive(Clone)]
 pub struct Store {
     pub db: Database,
+    pub auth_client: Client,
 }
