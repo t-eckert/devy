49b159fb30c6059939a59b5ab9d403e84c6587f5
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-02-18 15:11:28 -0500

    store: add provider for client

--- crates/store/src/lib.rs
+++ crates/store/src/lib.rs
@@ -1,10 +1,19 @@
+use auth::{Client, Provider};
 use db::Database;
-use auth::Client;
 
 /// Store is the shared state of the application.
 /// It contains a database pool, configuration, an authentication client, and an uploader.
 #[derive(Clone)]
-pub struct Store {
+pub struct Store<P> {
     pub db: Database,
-    pub auth_client: Client,
+    pub auth_client: Client<P>,
+}
+
+impl<P> Store<P>
+where
+    P: Provider,
+{
+    pub fn new(db: Database, auth_client: Client<P>) -> Self {
+        Self { db, auth_client }
+    }
 }
