ecfb4254e6e04991bdf74074bd67c70e18a67789
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-02-18 21:19:36 -0500

    router: rework for crate architecture

--- crates/router/src/router.rs
+++ crates/router/src/router.rs
@@ -1,12 +1,24 @@
-pub struct Router {}
+use axum::{routing::get, Router as AxumRouter};
+use std::net::SocketAddr;
+use store::Store;
 
+pub struct Router {
+    axum_router: AxumRouter,
+}
 
 impl Router {
-    pub fn new() -> Self {
-        Self{}
+    pub fn new(store: Store) -> Self {
+        let axum_router = AxumRouter::new()
+            .route("/ready", get(|| async { "OK" }))
+            .with_state(store);
+
+        Self { axum_router }
     }
 
     pub async fn serve(self) {
-        println!("Serving!");
+        let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
+
+        let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+        axum::serve(listener, self.axum_router).await.unwrap();
     }
 }
