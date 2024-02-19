344cfc5a51b3381992c31b54f635cb56484edd97
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-02-18 21:20:44 -0500

    api: use the new router

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -1,7 +1,19 @@
+use auth::{Client, Providers};
+use db::{connect, Config};
 use router::Router;
+use store::Store;
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new();
+    dotenvy::dotenv().ok();
+
+    let auth_client = Client::new(Providers::GitHub);
+
+    let config = Config::from_env().unwrap();
+    let db = connect(config).await.unwrap();
+
+    let store = Store::new(db, auth_client);
+
+    let router = Router::new(store);
     router.serve().await;
 }
