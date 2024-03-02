bec61cd65223f054248d3fb79b7788f7b73c3dc0
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-03-01 22:36:52 -0500

    Add tracing

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -3,9 +3,11 @@ use db::{connect, Config};
 use router::Router;
 use store::Store;
 
+/// Start the API server.
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
+    tracing::init();
 
     let auth_client = Client::new(Providers::GitHub);
 
