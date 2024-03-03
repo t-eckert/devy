cd4898110273fdc54108253e8ec9c290992312ca
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-03-02 23:59:31 -0500

    Work out auth providers

--- crates/auth/src/lib.rs
+++ crates/auth/src/lib.rs
@@ -1,9 +1,11 @@
 mod client;
+mod config;
 mod encoding_key;
 mod error;
 mod providers;
 
 pub use client::Client;
+pub use config::Config;
 pub use encoding_key::generate_encoding_key;
-pub use error::{Error, Result};
-pub use providers::Providers;
+pub use error::Error;
+pub use providers::Provider;
