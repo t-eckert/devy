85a0c18f123a2e58b0864f2f989d0371339f3ad4
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-03-04 21:59:24 -0500

    Redirect without the v1

--- site/src/routes/api/auth/signin/+server.ts
+++ site/src/routes/api/auth/signin/+server.ts
@@ -3,5 +3,5 @@ import { redirect } from "@sveltejs/kit"
 import config from "$lib/config"
 
 export const GET: RequestHandler = () => {
-	throw redirect(307, `${config.api}/v1/auth/login`)
+	throw redirect(307, `${config.api}/auth/login`)
 }
