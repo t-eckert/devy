816eeaf06dde21776fe8323f88a02e674d13bbb9
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-03-18 22:57:59 -0400

    site: leverage env vars for config

--- site/src/lib/config.ts
+++ site/src/lib/config.ts
@@ -1,15 +1,16 @@
 export interface Config {
-	version: string
 	host: string
 	api: string
-	mode: "development" | "production"
+}
+
+const localConfig: Config = {
+	host: "http://localhost:3000",
+	api: "http://localhost:8000"
 }
 
 const config: Config = {
-	version: "0.4.0",
-	host: "http://localhost:5173",
-	api: "http://localhost:8000",
-	mode: "development"
+	host: process.env.HOST ?? localConfig.host,
+	api: process.env.API ?? localConfig.api
 }
 
 export default config
