93803801e18c3caebc345892607bcc7a9d54e1bf
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-03-18 21:55:55 -0400

    site: render markdown

---
+++ notebook/tasks/fix-backend-deployment.md
@@ -0,0 +1,8 @@
+# Fix Backend Deployment
+
+Builds are broken on Fl0.
+
+This was due to misconfigured environment variables and the path for the
+Dockerfile being incorrect on `main`.
+
+Done!
--- site/src/routes/[blog]/[post]/+page.svelte
+++ site/src/routes/[blog]/[post]/+page.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
+	import SvelteMarkdown from "svelte-markdown"
+
 	import type { PageData } from "./$types"
 	import Main from "$lib/layout/main.svelte"
 	import H1 from "$lib/element/h1.svelte"
-	import Json from "$lib/utils/json.svelte"
 
 	export let data: PageData
 </script>
 
 <Main>
-	<article>{data.props.post.content}</article>
+	<div class="py-4 mx-auto w-full max-w-3xl">
+		<H1>{data.props.post.title}</H1>
+		<a href={`/${data.props.post.blogSlug}`}>{data.props.post.blogName}</a>
+		<a href={`/profiles/${data.props.post.authorUsername}`}>{data.props.post.authorName}</a>
+	</div>
+
+	<article class="mx-auto w-full max-w-3xl">
+		<SvelteMarkdown source={data.props.post.content} />
+	</article>
 </Main>
