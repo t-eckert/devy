edff0377840065f837b90579553d3ff4d90c2db2
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-01 22:16:35 -0400

    site: make the preview token orange

--- site/src/lib/header/home.svelte
+++ site/src/lib/header/home.svelte
@@ -1,7 +1,7 @@
 <div class="flex flex-row gap-2 items-baseline">
 	<a href="/" class="px-0.5 -ml-0.5 font-medium">Devy</a>
 	<span
-		class="text-xs font-medium px-2 py-0.5 rounded-full text-zinc-600 bg-zinc-200 dark:text-zinc-100 dark:bg-zinc-800 select-none"
+		class="text-xs font-medium px-2 py-0.5 rounded-full text-white bg-amber-600 dark:text-zinc-100 dark:bg-zinc-800 select-none"
 		>Preview</span
 	>
 </div>
