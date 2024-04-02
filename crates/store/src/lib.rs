510e0f703dfb579c35ef0095d1bc1dc4c95d0b29
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-01 21:41:06 -0400

    store: add uploader

--- crates/store/src/lib.rs
+++ crates/store/src/lib.rs
@@ -1,5 +1,6 @@
 use auth::Client;
 use db::Database;
+use uploads::Uploader;
 
 /// Store is the shared state of the application.
 /// It contains a database pool, an authentication client, and an uploader.
@@ -7,10 +8,15 @@ use db::Database;
 pub struct Store {
     pub db: Database,
     pub auth_client: Client,
+    pub uploader: Uploader,
 }
 
 impl Store {
-    pub fn new(db: Database, auth_client: Client) -> Self {
-        Self { db, auth_client }
+    pub fn new(db: Database, auth_client: Client, uploader: Uploader) -> Self {
+        Self {
+            db,
+            auth_client,
+            uploader,
+        }
     }
 }
