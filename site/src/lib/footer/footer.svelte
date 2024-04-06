56315955ff73d320ab60a1484474dba64c9501e6
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-06 15:30:46 -0400

    site: left-justify footer

--- site/src/lib/footer/footer.svelte
+++ site/src/lib/footer/footer.svelte
@@ -1,24 +1,26 @@
-<footer class="w-full transition-all pb-12">
-	<div class="mx-auto max-w-6xl p-3 pb-12 bg-zinc-200/40 dark:bg-zinc-900/40 rounded-xl">
-		<div class="flex flex-row gap-2 items-baseline mb-2">
-			<a href="/" class="px-0.5 -ml-0.5 font-medium">Devy</a>
-			<span
-				class="text-xs font-medium px-2 py-0.5 rounded-full text-zinc-600 bg-zinc-200 dark:text-zinc-100 dark:bg-zinc-800 select-none"
-				>Preview</span
-			>
-		</div>
-		<div class="pt-2 flex flex-col sm:flex-row gap-12 text-sm text-zinc-800 dark:text-zinc-300">
-			<section class="w-full sm:w-56 flex flex-col gap-4">
-				<p>
-					Devy is an open source platform for people to publish blog posts in markdown from their
-					GitHub repositories.{" "}
-				</p>
-				<p>Thank you for checking it out.</p>
-				<span class="self-end">
-					&#8212;{" "}
-					<a href="https://thomaseckert.dev">Thomas Eckert</a>
-				</span>
-			</section>
+<footer class="w-full transition-all py-12">
+	<div class="mx-auto max-w-6xl flex flex-row items-start">
+		<div class="p-3 pb-12 bg-zinc-200/40 dark:bg-zinc-900/40 rounded-xl">
+			<div class="flex flex-row gap-2 items-baseline mb-2">
+				<a href="/" class="px-0.5 -ml-0.5 font-medium">Devy</a>
+				<span
+					class="text-xs font-medium px-2 py-0.5 rounded-full text-white bg-amber-600 dark:text-zinc-100 dark:bg-zinc-800 select-none"
+					>Preview</span
+				>
+			</div>
+			<div class="pt-2 flex flex-col sm:flex-row gap-12 text-sm text-zinc-800 dark:text-zinc-300">
+				<section class="w-full sm:w-56 flex flex-col gap-4">
+					<p>
+						Devy is an open source platform for people to publish blog posts in markdown from their
+						GitHub repositories.{" "}
+					</p>
+					<p>Thank you for checking it out.</p>
+					<span class="self-end">
+						&#8212;{" "}
+						<a href="https://thomaseckert.dev">Thomas Eckert</a>
+					</span>
+				</section>
+			</div>
 		</div>
 	</div>
 </footer>
