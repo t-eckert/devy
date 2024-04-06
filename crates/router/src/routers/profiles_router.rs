3bbffc2b1ddd27fdb6ecd7ea1a61237a68b1296e
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-05 23:07:36 -0400

    router: clean up profiles_router

--- crates/router/src/routers/profiles_router.rs
+++ crates/router/src/routers/profiles_router.rs
@@ -4,45 +4,24 @@ use axum::{
     routing::get,
     Json, Router,
 };
-use db::{blog, like, post, profile};
-use entities::{Blog, Like, Post, Profile};
+use db::{blog, like, profile};
+use entities::{Blog, Like, Profile};
 use store::Store;
-use uuid::Uuid;
 
 pub struct ProfilesRouter;
 
+/// `/profiles`
 impl ProfilesRouter {
     pub fn create(store: Store) -> Router<Store> {
         Router::new()
             .route("/profiles/:username", get(get_profile_by_username))
             .route("/profiles/:username/blogs", get(get_blogs_by_username))
-            .route("/profiles/:username/posts", get(get_posts_by_username))
             .with_state(store)
     }
 }
 
-/// Get a post
-async fn get_by_post_id(
-    State(store): State<Store>,
-    Path(post_id): Path<Uuid>,
-) -> Result<Json<Post>> {
-    Ok(Json(post::get_by_id(&store.db, post_id).await?))
-}
-
-async fn get_by_blog_slug(
-    State(store): State<Store>,
-    Path(blog_slug): Path<String>,
-) -> Result<Json<Vec<Post>>> {
-    Ok(Json(post::get_by_blog_slug(&store.db, &blog_slug).await?))
-}
-
-async fn get_posts_by_username(
-    State(store): State<Store>,
-    Path(username): Path<String>,
-) -> Result<Json<Vec<Post>>> {
-    Ok(Json(post::get_by_username(&store.db, &username).await?))
-}
-
+/// `GET /profiles/:username`
+///
 /// Get a profile by username.
 async fn get_profile_by_username(
     State(store): State<Store>,
@@ -51,7 +30,7 @@ async fn get_profile_by_username(
     Ok(Json(profile::get_by_username(&store.db, username).await?))
 }
 
-/// GET /profiles/:username/blogs
+/// `GET /profiles/:username/blogs`
 ///
 /// Get all blogs for a profile by username.
 async fn get_blogs_by_username(
@@ -59,11 +38,4 @@ async fn get_blogs_by_username(
     Path(username): Path<String>,
 ) -> Result<Json<Vec<Blog>>> {
     Ok(Json(blog::get_by_username(&store.db, username).await?))
-}
-
-async fn get_ids_by_username(
-    State(store): State<Store>,
-    Path(username): Path<String>,
-) -> Result<Json<Vec<Like>>> {
-    Ok(Json(like::get_by_username(&store.db, username).await?))
 }
