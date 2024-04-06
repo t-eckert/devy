6e537d2cd583ddf69c3ed20f51167403e7814784
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-05 23:23:22 -0400

    db: add session actions

--- db/src/session/actions.rs
+++ db/src/session/actions.rs
@@ -1,11 +1,27 @@
-// use crate::{error::Result, Database};
-//
-// pub fn insert(db: &Database, session: entities::Session) -> Result<()> {
-//     Ok(sqlx::query_file!(
-//         "src/feed_config_user/queries/insert.sql",
-//         user_id,
-//         feed_config_id
-//     )
-//     .execute(db)
-//     .await?)
-// }
+use crate::{error::Result, Database};
+use entities::SessionMetadata;
+use uuid::Uuid;
+
+pub async fn insert(
+    db: &Database,
+    user_id: Uuid,
+    metadata: SessionMetadata,
+    token: String,
+    exp: i32,
+    access_token: String,
+    encoding_key: String,
+) -> Result<()> {
+    sqlx::query_file!(
+        "src/session/queries/insert.sql",
+        user_id,
+        serde_json::to_value(metadata).unwrap_or_default(),
+        token,
+        exp,
+        access_token,
+        encoding_key
+    )
+    .execute(db)
+    .await?;
+
+    Ok(())
+}
---
+++ db/src/session/queries/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO "session" (user_id, metadata, token, exp, access_token, encoding_key)
+VALUES ($1, $2, $3, $4, $5, $6);
