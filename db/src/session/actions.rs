186a7d263200bc71bb5517b7c75dbb67af9feecb
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-06 12:04:18 -0400

    db: insert sessions

--- db/src/session/actions.rs
+++ db/src/session/actions.rs
@@ -1,5 +1,5 @@
 use crate::{error::Result, Database};
-use entities::SessionMetadata;
+use entities::{Session, SessionMetadata};
 use uuid::Uuid;
 
 pub async fn insert(
@@ -7,21 +7,18 @@ pub async fn insert(
     user_id: Uuid,
     metadata: SessionMetadata,
     token: String,
-    exp: i32,
     access_token: String,
     encoding_key: String,
-) -> Result<()> {
-    sqlx::query_file!(
+) -> Result<Session> {
+    Ok(sqlx::query_file_as!(
+        Session,
         "src/session/queries/insert.sql",
         user_id,
         serde_json::to_value(metadata).unwrap_or_default(),
         token,
-        exp,
         access_token,
         encoding_key
     )
-    .execute(db)
-    .await?;
-
-    Ok(())
+    .fetch_one(db)
+    .await?)
 }
--- db/src/session/queries/insert.sql
+++ db/src/session/queries/insert.sql
@@ -1,2 +1,12 @@
-INSERT INTO "session" (user_id, metadata, token, exp, access_token, encoding_key)
-VALUES ($1, $2, $3, $4, $5, $6);
+INSERT INTO "session" (user_id, metadata, token, access_token, encoding_key)
+VALUES ($1, $2, $3, $4, $5)
+RETURNING
+    id,
+    user_id,
+    metadata,
+    token,
+    exp,
+    access_token,
+    encoding_key,
+    to_char("session".created_at, 'YYYY-MM-DDThh:mm:ss.ss') AS created_at,
+    to_char("session".last_used_at, 'YYYY-MM-DDThh:mm:ss.ss') AS last_used_at;
