e834c98c53f4eab2d6446adc430979a553b575ac
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-11 22:38:27 -0400

    entities: add feed_metadata to feed

--- crates/entities/src/feed.rs
+++ crates/entities/src/feed.rs
@@ -1,8 +1,10 @@
 use crate::Entry;
+use crate::FeedConfig;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Feed {
+    pub feed_config: FeedConfig,
     pub entries: Vec<Entry>,
 }
--- crates/entities/src/feed_config.rs
+++ crates/entities/src/feed_config.rs
@@ -1,15 +1,17 @@
 use serde::{Deserialize, Serialize};
-use uuid::Uuid;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct FeedConfig {
-    pub id: Uuid,
+    pub id: String,
     pub name: String,
 }
 
 impl FeedConfig {
-    pub fn new(id: Uuid, name: String) -> Self {
-        Self { id, name }
+    pub fn new(id: &str, name: &str) -> Self {
+        Self {
+            id: id.to_string(),
+            name: name.to_string(),
+        }
     }
 }
