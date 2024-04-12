b307c9bc64ea3b5f80a5797040941b989f4b5c8c
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-11 22:37:25 -0400

    db: add user metadata

--- db/src/feed/actions.rs
+++ db/src/feed/actions.rs
@@ -1,8 +1,9 @@
 use crate::{error::Result, Database};
-use entities::{Entry, Feed};
+use entities::{Entry, Feed, FeedConfig};
 
 pub async fn get_recent(db: &Database) -> Result<Feed> {
     Ok(Feed {
+        feed_config: FeedConfig::new("recent", "Recent"),
         entries: sqlx::query_file_as!(Entry, "src/feed/queries/get_recent.sql")
             .fetch_all(db)
             .await?,
