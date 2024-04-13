89f86f3b8a80fa0a4b06ad17a4882badb465fa51
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-13 16:26:22 -0400

    uploads: return 404 when a repo does not exist

--- crates/uploads/src/error.rs
+++ crates/uploads/src/error.rs
@@ -5,15 +5,6 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// Errors that can occur during authentication processes.
 #[derive(Debug, Serialize)]
 pub enum Error {
-    /// GitHub returned an error when exchanging the code for a token.
-    CodeExchangeForTokenFailed(String),
-
-    /// GitHub returned an error when exchanging the token for a user.
-    TokenExchangeForUserFailed(String),
-
-    /// The GitHub user data could not be deserialized.
-    UnableToDeserializeUser(String),
-
     DependencyError(String),
 
     RepositoryNotFound(String),
@@ -30,13 +21,17 @@ pub enum Error {
 
 impl From<entities::Error> for Error {
     fn from(err: entities::Error) -> Self {
-        Error::TokenExchangeForUserFailed(err.to_string())
+        match err {
+            _ => Error::DependencyError(err.to_string()),
+        }
     }
 }
 
 impl From<db::Error> for Error {
     fn from(err: db::Error) -> Self {
-        Error::DependencyError(err.to_string())
+        match err {
+            _ => Error::DependencyError(err.to_string()),
+        }
     }
 }
 
