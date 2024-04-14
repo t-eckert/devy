7db2235b975991c42b8bf444b91b470e03a9ec9c
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-14 17:42:36 -0400

    site: add Sentry

--- site/src/hooks.server.ts
+++ site/src/hooks.server.ts
@@ -1,6 +1,15 @@
+import { sequence } from "@sveltejs/kit/hooks"
+import * as Sentry from "@sentry/sveltekit"
 import type { Handle } from "@sveltejs/kit"
 import authHandle from "$lib/auth/hook"
 
-export const handle: Handle = async ({ event, resolve }) => {
+Sentry.init({
+	dsn: "https://f287c7e744498611eecc5ddb1f86a25b@o4506961359536128.ingest.us.sentry.io/4507087061450752",
+	tracesSampleRate: 1
+})
+
+export const handle: Handle = sequence(Sentry.sentryHandle(), async ({ event, resolve }) => {
 	return authHandle({ event, resolve })
-}
+})
+
+export const handleError = Sentry.handleErrorWithSentry()
--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,8 +1,14 @@
+import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sentrySvelteKit({
+        sourceMapsUploadOptions: {
+            org: "thomas-eckert",
+            project: "devy-site"
+        }
+    }), sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
