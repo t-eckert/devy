f708a7bdaa254e51720a4e03614731ebd4376e85
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-14 00:10:49 -0400

    site: add Inter and Space Mono

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -3,6 +3,10 @@ export default {
 	darkMode: "class",
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 	theme: {
+		fontFamily: {
+			sans: ["Inter", "sans-serif"],
+			mono: ["Space Mono", "monospace"]
+		},
 		extend: {
 			listStyleType: {
 				current: "url('/chevron.svg')"
