52898191b15e8aee65cbaceb96cb253fb4a4c9f8
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-14 21:43:24 -0400

    profiles: show entries

--- site/src/routes/profiles/[username]/+page.server.ts
+++ site/src/routes/profiles/[username]/+page.server.ts
@@ -1,17 +1,19 @@
 import type { PageServerLoad } from "./$types"
 import api from "$lib/api"
-import type { User, Blog, Profile } from "$lib/types"
+import type { User, Blog, Profile, Entry } from "$lib/types"
 
 export const load: PageServerLoad = async ({ params }) => {
+	const user = await api.get<User>(`/users/${params.username}`)
 	const profile = await api.get<Profile>(`/profiles/${params.username}`)
 	const blogs = await api.get<Blog[]>(`/profiles/${params.username}/blogs`)
-	const user = await api.get<User>(`/users/${params.username}`)
+	const entries = await api.get<Entry[]>(`/profiles/${params.username}/entries`)
 
 	return {
 		props: {
 			profile,
 			blogs,
-			user
+			user,
+			entries
 		}
 	}
 }
