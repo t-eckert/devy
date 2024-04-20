39cd8238fc78d6e05a7b3e0db51ed10fedbd634a
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-20 14:04:06 -0400

    logo: add logo

--- site/src/lib/components/header/home.svelte
+++ site/src/lib/components/header/home.svelte
@@ -1,5 +1,9 @@
+<script lang="ts">
+	import Logo from "$lib/components/logo/logo.svelte"
+</script>
+
 <div class="flex flex-row gap-2 items-center">
-	<a href="/" class="px-0.5 -ml-0.5 font-medium">Devy</a>
+	<a href="/" class="px-0.5 -ml-0.5"><Logo height={20} /></a>
 	<span
 		class="text-xs font-medium px-2 py-0.5 rounded-full text-white bg-amber-600 dark:text-zinc-100 dark:bg-zinc-800 select-none"
 		>Preview</span
