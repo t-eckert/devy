326183f239ea8f0386eb2b2f79dc9162d4f86397
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-20 14:07:03 -0400

    site: fix some full width layout issues

--- site/src/lib/components/sidebar/sidebar.svelte
+++ site/src/lib/components/sidebar/sidebar.svelte
@@ -4,7 +4,7 @@
 
 <section class="flex flex-col gap-4 text-sm">
 	<div>
-		<p class="font-medium">Welcome to Devy!</p>
+		<h2 class="font-medium text-xl mb-1">Welcome to Devy!</h2>
 		<p>Devy is a blogging playform powered by GitHub and Markdown.</p>
 	</div>
 	<Onboarding />
