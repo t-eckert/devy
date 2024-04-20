42d20029f593563817d7b9f4ca8ef13f736e4cfa
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-20 12:46:52 -0400

    router: add likes endpoints

--- crates/router/src/routers/likes_router.rs
+++ crates/router/src/routers/likes_router.rs
@@ -1,39 +1,45 @@
-use super::error::Result;
-use crate::{
-    entities::{like, Like},
-    store::Store,
-};
+use crate::error::Result;
 use axum::{
     extract::{Json as ExtractJson, Path, State},
-    routing::{delete, post},
+    routing::{delete, get, post},
     Json,
 };
+use db::like;
+use entities::Like;
+use store::Store;
 use uuid::Uuid;
 
-pub fn make_router(store: Store) -> axum::Router<Store> {
-    axum::Router::new()
-        .route("/likes", post(post_like))
-        .route("/likes/:post_id/:profile_id", delete(delete_like))
-        .with_state(store)
+pub struct LikesRouter;
+
+impl LikesRouter {
+    pub fn create(store: Store) -> axum::Router<Store> {
+        axum::Router::new()
+            .route("/likes", post(post_like))
+            .route("/likes/:username", get(get_by_username))
+            .route("/likes/:profile_id/:post_id", delete(delete_like))
+            .with_state(store)
+    }
 }
 
-async fn get_ids_by_username(
+async fn get_by_username(
     State(store): State<Store>,
     Path(username): Path<String>,
 ) -> Result<Json<Vec<Like>>> {
-    Ok(Json(like::get_by_username(&store.pool, username).await?))
+    Ok(Json(like::get_by_username(&store.db, username).await?))
 }
 
 async fn post_like(
     State(store): State<Store>,
-    ExtractJson(like): ExtractJson<like::LikeForUpsert>,
+    ExtractJson(like): ExtractJson<Like>,
 ) -> Result<Json<Like>> {
-    Ok(Json(like::upsert(&store.pool, like).await?))
+    Ok(Json(
+        like::upsert(&store.db, like.profile_id, like.post_id).await?,
+    ))
 }
 
 async fn delete_like(
     State(store): State<Store>,
-    Path((post_id, profile_id)): Path<(Uuid, Uuid)>,
+    Path((profile_id, post_id)): Path<(Uuid, Uuid)>,
 ) -> Result<()> {
-    Ok(like::delete(&store.pool, profile_id, post_id).await?)
+    Ok(like::delete(&store.db, profile_id, post_id).await?)
 }
