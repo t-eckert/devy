107e45c0bca1c586b72897457353765e6ae620f8
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-20 14:04:06 -0400

    logo: add logo

---
+++ site/src/lib/components/logo/logo.stories.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+	import Logo from "./logo.svelte"
+
+	export const meta = {
+		title: "components/Logo",
+		component: Logo
+	}
+</script>
+
+<script lang="ts">
+	import { Template, Story } from "@storybook/addon-svelte-csf"
+</script>
+
+<Template let:args>
+	<Logo {...args} />
+</Template>
+
+<Story name="Basic Logo" args={{ height: 100 }}></Story>
