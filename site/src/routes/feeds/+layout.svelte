8e146dc5cfc95769ea57ecb4c2f10c2af37bbd1d
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-27 17:58:36 -0400

    feeds: style active feed

--- site/src/routes/feeds/+layout.svelte
+++ site/src/routes/feeds/+layout.svelte
@@ -2,6 +2,7 @@
 	import Main from "$lib/layouts/main.svelte"
 	import Sidebar from "$lib/components/sidebar/sidebar.svelte"
 	import FeedSelector from "$lib/components/feed-selector/feed-selector.svelte"
+	import { page } from "$app/stores"
 
 	const feedConfigs = [
 		{
@@ -9,13 +10,15 @@
 			title: "Recent"
 		}
 	]
+
+	const selectedFeed = $page.params.feed
 </script>
 
 <Main>
 	<div class="sm:py-4 grid grid-cols-1 sm:grid-cols-4 gap-6 items-start">
 		<div class="col-start-1 sm:col-span-3">
 			<div class="flex flex-col sm:flex-row gap-6">
-				<FeedSelector {feedConfigs} />
+				<FeedSelector {feedConfigs} {selectedFeed} />
 				<slot />
 			</div>
 		</div>
