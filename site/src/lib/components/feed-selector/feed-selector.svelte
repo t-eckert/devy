c3c95d653cd4088f85b30766f69b52365574d692
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-27 17:58:36 -0400

    feeds: style active feed

--- site/src/lib/components/feed-selector/feed-selector.svelte
+++ site/src/lib/components/feed-selector/feed-selector.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
 	export let feedConfigs: any
+	export let selectedFeed: string
+
+	const base =
+		"text-sm px-2 py-1 sm:w-52 flex flex-row items-start font-medium rounded-lg border hover:pl-4 transition-all"
+	const selected = "bg-zinc-50 text-stone-950"
+	const notSelected = "text-stone-700 border-transparent"
+	const style = (id: string, selectedId: string): string => {
+		return base + (id === selectedId ? ` ${selected}` : ` ${notSelected}`)
+	}
 </script>
 
 <div class="flex flex-col col-start-1 sm:flex-col items-start gap-2">
 	{#each feedConfigs as feedConfig}
-		<a
-			href={`/feeds/${feedConfig.id}`}
-			class={[
-				"bg-stone-50 text-sm  px-2 py-1 sm:w-52 flex flex-row items-start font-medium rounded-lg border hover:pl-4 transition-all",
-				"text-stone-900 border-stone-200 shadow-xs"
-			].join(" ")}>{feedConfig.title}</a
+		<a href={`/feeds/${feedConfig.id}`} class={style(feedConfig.id, selectedFeed)}
+			>{feedConfig.title}</a
 		>
 	{/each}
 </div>
