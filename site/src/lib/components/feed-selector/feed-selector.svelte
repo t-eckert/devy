3ee5d3aae2886f5442e830b2c9ac23b71af74d0c
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-27 01:38:27 -0400

    feeds: separate out feed selector

---
+++ site/src/lib/components/feed-selector/feed-selector.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let feedConfigs: any
+</script>
+
+<div class="flex flex-col col-start-1 sm:flex-col items-start gap-2">
+	{#each feedConfigs as feedConfig}
+		<a
+			href={`/feeds/${feedConfig.id}`}
+			class={[
+				"bg-stone-50 text-sm  px-2 py-1 sm:w-52 flex flex-row items-start font-medium rounded-lg border hover:pl-4 transition-all",
+				"text-stone-900 border-stone-200 shadow-xs"
+			].join(" ")}>{feedConfig.title}</a
+		>
+	{/each}
+</div>
--- site/src/routes/feeds/[feed]/+page.ts
+++ site/src/routes/feeds/[feed]/+page.ts
@@ -1,8 +1,15 @@
 import { PageLoad } from "./$types"
 import type { Feed } from "$lib/types"
+import { NumericRange, error } from "@sveltejs/kit"
 
 export const load: PageLoad = async ({ fetch, params }) => {
 	const resp = await fetch(`/api/feeds/${params.feed}`)
+	if (!resp.ok) {
+		throw error(resp.status as NumericRange<400, 509>, resp.statusText)
+	}
+
+	console.log(resp.status)
+
 	const feed = (await resp.json()) as Feed
 
 	return {
