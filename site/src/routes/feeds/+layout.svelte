885beb62f725e43168378f1332cc48244778c9ed
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-04-27 01:38:27 -0400

    feeds: separate out feed selector

--- site/src/routes/feeds/+layout.svelte
+++ site/src/routes/feeds/+layout.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
 	import Main from "$lib/layouts/main.svelte"
 	import Sidebar from "$lib/components/sidebar/sidebar.svelte"
+	import FeedSelector from "$lib/components/feed-selector/feed-selector.svelte"
+
+	const feedConfigs = [
+		{
+			id: "recent",
+			title: "Recent"
+		}
+	]
 </script>
 
 <Main>
 	<div class="sm:py-4 grid grid-cols-1 sm:grid-cols-4 gap-6 items-start">
 		<div class="col-start-1 sm:col-span-3">
-			<div class="flex flex-row gap-4">
-				<div class="flex flex-col col-start-1 sm:flex-col items-start gap-2">
-					<a
-						href="/feeds/recent"
-						class={[
-							"bg-stone-50 text-sm  px-2 py-1 sm:w-52 flex flex-row items-start font-medium rounded-lg border hover:pl-4 transition-all",
-							"text-stone-900 border-stone-200 shadow-xs"
-						].join(" ")}>Recent</a
-					>
-				</div>
+			<div class="flex flex-col sm:flex-row gap-6">
+				<FeedSelector {feedConfigs} />
 				<slot />
 			</div>
 		</div>
