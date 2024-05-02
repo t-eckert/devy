ef0e129f4c47702d64afde90df789e8b0ef68134
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-05-01 22:55:11 -0400

    site: fix forwarding of api requests

--- site/src/routes/api/[...route]/+server.ts
+++ site/src/routes/api/[...route]/+server.ts
@@ -1,7 +1,33 @@
 import config from "$lib/config"
 import type { RequestHandler } from "./$types"
 
+const forwardingHeaders = new Set([
+	"accept",
+	"accept-encoding",
+	"accept-language",
+	"cache-control",
+	"pragma",
+	"referer",
+	"user-agent",
+	"x-requested-with",
+	"cookie",
+	"authorization",
+	"content-type",
+	"content-length",
+	"origin"
+])
+
 export const fallback: RequestHandler = async ({ url, request }) => {
 	const apiPath = url.pathname.replace("/api", "")
-	return await fetch(config.api + apiPath, request)
+	const { method, headers: incomingHeaders } = request
+
+	const headers = new Headers()
+	incomingHeaders.forEach((value, key) => {
+		if (forwardingHeaders.has(key)) headers.set(key, value)
+	})
+
+	return await fetch(config.api + apiPath, {
+		method,
+		headers
+	})
 }
