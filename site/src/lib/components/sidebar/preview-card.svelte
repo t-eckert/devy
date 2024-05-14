3056fcfce4656f33df4112925f896bf0744f2ce5
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-05-14 13:27:53 -0400

    sidebar: point the user to the correct next step based on their onboarding state

---
+++ site/src/lib/components/sidebar/preview-card.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Card from "$lib/components/card/card.svelte"
+</script>
+
+<Card
+	styling="p-2 bg-orange-100 text-orange-900 border-orange-200 dark:bg-orange-950 dark:text-orange-300 dark:border-orange-800 flex flex-col gap-2"
+>
+	<p>
+		Devy is still in Preview. Thank you for checking it out while it's still in its early stages.
+	</p>
+	<p>
+		Sharing your <a href="/feedback" class="underline font-medium">feedback</a> is an amazing way you
+		can help make this site better.
+	</p>
+</Card>
