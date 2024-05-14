64bf0061396ae0f231f0cf912627d1906e22c044
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-05-14 13:13:42 -0400

    router: add user github endpoints

--- lib/src/router/routers/users_router.rs
+++ lib/src/router/routers/users_router.rs
@@ -1,12 +1,13 @@
-use crate::db::user;
 use crate::entities::User;
 use crate::router::error::Result;
 use crate::store::Store;
+use crate::{db::user, router::Error};
 use axum::{
     extract::{Path, State},
     routing::get,
     Json, Router,
 };
+use serde_json::Value;
 
 pub struct UsersRouter;
 
@@ -14,6 +15,8 @@ impl UsersRouter {
     pub fn create(store: Store) -> Router<Store> {
         Router::new()
             .route("/users/:username", get(get_by_username))
+            .route("/users/:username/github/repos", get(get_user_github_repos))
+            .route("/users/:username/github/devy", get(get_user_github_devy))
             .with_state(store)
     }
 }
@@ -26,4 +29,34 @@ async fn get_by_username(
     Path(username): Path<String>,
 ) -> Result<Json<User>> {
     Ok(Json(user::get_by_username(&store.db, &username).await?))
+}
+
+/// `GET /users/:username/github/repos`
+///
+/// Get a user's GitHub repositories.
+async fn get_user_github_repos(Path(username): Path<String>) -> Result<Json<Value>> {
+    match reqwest::Client::new()
+        .get(&format!(
+            "https://api.github.com/users/{}/repos?per_page=100&sort=updated",
+            username
+        ))
+        .header("User-Agent", "devy")
+        .header("Accept", "application/json")
+        .send()
+        .await
+    {
+        Ok(response) => {
+            Ok(Json(response.json().await.map_err(|_| {
+                Error::Generic("Request to GitHub Failed".to_string())
+            })?))
+        }
+        Err(_) => Err(Error::Generic("Request to GitHub Failed".to_string())),
+    }
+}
+
+/// `GET /users/:username/github/devy`
+///
+/// Get a user's GitHub Devy metadata.
+async fn get_user_github_devy(Path(username): Path<String>) -> Result<Json<()>> {
+    Ok(Json(()))
 }
