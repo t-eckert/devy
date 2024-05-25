a7f1801eb2f96c3d41a6bf777715204986c7e3ea
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-05-24 22:45:01 -0400

    database: simplify configuration of connection

--- lib/src/db/database.rs
+++ lib/src/db/database.rs
@@ -1,16 +1,14 @@
-use crate::db::Config;
 use crate::db::Result;
 use sqlx::postgres::PgPoolOptions;
 use std::time::Duration;
 
 pub type Database = sqlx::PgPool;
 
-pub async fn connect(config: Config) -> Result<Database> {
-    let connection_string = config.database_url;
+pub async fn connect(database_url: &str) -> Result<Database> {
     let pool = PgPoolOptions::new()
         .max_connections(5)
         .acquire_timeout(Duration::from_secs(30))
-        .connect(&connection_string)
+        .connect(database_url)
         .await?;
     sqlx::migrate!().run(&pool).await.unwrap();
 
--- lib/src/db/mod.rs
+++ lib/src/db/mod.rs
@@ -1,8 +1,6 @@
-mod config;
 mod database;
 mod error;
 
-pub use config::Config;
 pub use database::{connect, Database};
 pub use error::{Error, Result};
 
