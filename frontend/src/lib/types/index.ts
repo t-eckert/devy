d95def104555ebe6e4698996ebc31fc006320c45
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-07-08 21:38:41 -0400

    frontend:types: add Notification

--- frontend/src/lib/types/index.ts
+++ frontend/src/lib/types/index.ts
@@ -1,16 +1,17 @@
-import type Blog from "./Blog"
-import type Bookmark from "./Bookmark"
-import type Entry from "./Entry"
-import type Feed from "./Feed"
-import type FeedConfig from "./FeedConfig"
-import type Like from "./Like"
-import type Post from "./Post"
-import type Profile from "./Profile"
-import type Repo from "./Repo"
-import type Session from "./Session"
-import type TokenPayload from "./TokenPayload"
-import type Upload from "./Upload"
-import type User from "./User"
+import type Blog from './Blog';
+import type Bookmark from './Bookmark';
+import type Entry from './Entry';
+import type Feed from './Feed';
+import type FeedConfig from './FeedConfig';
+import type Like from './Like';
+import type Notification from './Notification';
+import type Post from './Post';
+import type Profile from './Profile';
+import type Repo from './Repo';
+import type Session from './Session';
+import type TokenPayload from './TokenPayload';
+import type Upload from './Upload';
+import type User from './User';
 
 export type {
 	Blog,
@@ -19,6 +20,7 @@ export type {
 	Feed,
 	FeedConfig,
 	Like,
+	Notification,
 	Post,
 	Profile,
 	Repo,
@@ -26,4 +28,4 @@ export type {
 	TokenPayload,
 	Upload,
 	User
-}
+};
