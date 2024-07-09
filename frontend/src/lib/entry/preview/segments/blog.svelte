e09f9f3b3e0959f54db688b8e845c1c2a52c7f06
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-07-08 21:37:53 -0400

    frontend: remove old entry preview

--- frontend/src/lib/entry/preview/segments/blog.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	export let blogName: string
-	export let blogSlug: string
-</script>
-
-<a href={`/${blogSlug}`} class="text-xs text-stone-500 dark:text-stone-400">{blogName}</a>
--- frontend/src/lib/entry/preview/segments/date.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import RelativeDate from "$lib/utils/relative-date.svelte"
-	export let date: string
-</script>
-
-<div class="text-xs dark:text-stone-300"><RelativeDate {date} /></div>
