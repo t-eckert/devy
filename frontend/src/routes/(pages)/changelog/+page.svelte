881e1648b8409ec9a44cb0ecfb77cbc9a40025a9
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-07-13 23:19:23 -0400

    changelog: increase padding between versions

--- frontend/src/routes/(pages)/changelog/+page.svelte
+++ frontend/src/routes/(pages)/changelog/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { PageData } from "./$types"
 	import H1 from "$lib/elements/h1.svelte"
+	import P from "$lib/elements/p.svelte"
 	import Card from "$lib/components/card.svelte"
 	import Markdown from "$lib/markdown/markdown.svelte"
 
@@ -11,9 +12,15 @@
 	<title>Changelog</title>
 </svelte:head>
 
-<div class="flex flex-col gap-4 w-full max-w-3xl mx-auto">
-	<div class="mb-4 mt-8">
+<div class="flex flex-col gap-6 w-full max-w-3xl mx-auto">
+	<div class="mt-8 flex flex-col gap-6">
 		<H1>Changelog</H1>
+		<div class="max-w-sm">
+			<P>
+				While Devy is released continuously, these changelog entries tag notable changes to the
+				project.
+			</P>
+		</div>
 	</div>
 	{#each data.props.changelogs as changelog}
 		<Card>
