a7d619dfdf3d8639f3aa202ec75d2458bba5135f
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-07-23 22:53:22 -0400

    tools: make req a tool

--- tools/req.py
+++ tools/req.py
@@ -1,7 +1,11 @@
 from rich import print
-from framework.router import route
 
 import argparse
+import httpx
+import os
+
+LOCAL = "http://localhost:3000"
+
 
 parser = argparse.ArgumentParser(description="Request a route.")
 parser.add_argument("method", type=str, help="HTTP method.")
@@ -9,12 +13,20 @@ parser.add_argument("path", type=str, help="Route path.")
 parser.add_argument("--payload", type=str, help="Payload to send.")
 parser.add_argument("--session", type=str, help="Session to use.")
 
-if __name__ == "__main__":
+
+def main():
+    client = httpx.Client(
+        base_url=os.environ.get("URL") or LOCAL, follow_redirects=True
+    )
     args = parser.parse_args()
-    request = route(args.method, args.path)
-    response = request()
+    response = client.request(args.method, args.path)
+
     print(response.status_code)
     if response.headers["Content-Type"] == "application/json":
         print(response.json())
     elif response.headers["Content-Type"] == "text/html":
         print(response.text)
+
+
+if __name__ == "__main__":
+    main()
