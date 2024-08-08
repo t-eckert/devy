2ada097e02de40971d4d3681655c37ee94d03ddc
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-08-08 17:03:13 -0400

    dialog: use web standard dialog component

--- frontend/src/lib/components/dialog.svelte
+++ frontend/src/lib/components/dialog.svelte
@@ -1,5 +1,5 @@
-<div
-	class="w-screen h-screen overflow-none flex justify-center items-center top-0 left-0 fixed z-50 backdrop-blur"
+<dialog
+	class="w-screen h-screen overflow-none flex justify-center items-center top-0 left-0 fixed z-50 bg-stone-200/20 backdrop-blur-sm"
 >
 	<slot />
-</div>
+</dialog>
