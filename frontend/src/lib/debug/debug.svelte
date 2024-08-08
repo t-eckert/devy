f2b936c7b82f0bc0de241c9e45a8e6cc76569d57
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-08-08 17:05:32 -0400

    debug: build out a debug panel for the frontend

---
+++ frontend/src/lib/debug/debug.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import Button from "$lib/components/button.svelte"
+	import Card from "$lib/components/card.svelte"
+	import Dialog from "$lib/components/dialog.svelte"
+
+	import Reader from "$lib/icons/reader.svelte"
+	import Cross from "$lib/icons/cross-1.svelte"
+
+	import Likes from "./likes.svelte"
+	import Session from "./session.svelte"
+
+	let isOpen = $state(false)
+</script>
+
+{#if isOpen}
+	<Dialog>
+		<div class="w-full max-w-6xl">
+			<Card>
+				<Button
+					onclick={() => {
+						isOpen = false
+					}}><Cross /></Button
+				>
+				<Likes />
+				<Session />
+			</Card>
+		</div>
+	</Dialog>
+{:else}
+	<div class="z-50 fixed bottom-3 right-3">
+		<Button
+			onclick={() => {
+				isOpen = true
+			}}><Reader /></Button
+		>
+	</div>
+{/if}
---
+++ frontend/src/lib/debug/session.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { getSessionState } from "$lib/state/session.svelte"
+	import Json from "$lib/utils/json.svelte"
+
+	const session = getSessionState()
+</script>
+
+<Json data={session.__debug()} />
