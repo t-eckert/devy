644e52080a4afd71d4995203c3681ca34b9a6c93
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-08-08 22:17:43 -0400

    api: expose popular feed

--- api/src/router/endpoints/feeds.rs
+++ api/src/router/endpoints/feeds.rs
@@ -5,10 +5,16 @@ use lib::{db::feed, entities::Feed, store::Store};
 pub fn router(store: Store) -> axum::Router<Store> {
     axum::Router::new()
         .route("/feeds/recent", get(get_recent))
+        .route("/feeds/popular", get(get_popular))
         .with_state(store)
 }
 
 // GET /feeds/recent
 async fn get_recent(State(store): State<Store>) -> Result<Json<Feed>> {
     Ok(Json(feed::get_recent(&store.db).await?))
+}
+
+// GET /feeds/popular
+async fn get_popular(State(store): State<Store>) -> Result<Json<Feed>> {
+    Ok(Json(feed::get_popular(&store.db).await?))
 }
