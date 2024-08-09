9a52f2cfa10cbeeebcb7e22122bd43ea34d0452a
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-08-08 23:38:24 -0400

    auth: use static PUBLIC_API

--- frontend/src/routes/(pages)/auth/sign-in/+page.ts
+++ frontend/src/routes/(pages)/auth/sign-in/+page.ts
@@ -1,6 +1,6 @@
 import { redirect } from "@sveltejs/kit"
-import { env } from "$env/dynamic/public"
+import { PUBLIC_API } from "$env/static/public"
 
 export function load() {
-	return redirect(307, `${env.PUBLIC_API}/auth/sign-in`)
+	return redirect(307, `${PUBLIC_API}/auth/sign-in`)
 }
