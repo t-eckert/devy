4e86da88460dfc72fe7d71553e905f4efb57bde9
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-08-14 23:54:11 -0400

    profiles: use PUBLIC_API

--- frontend/src/routes/(pages)/profiles/[username]/+page.svelte
+++ frontend/src/routes/(pages)/profiles/[username]/+page.svelte
@@ -1,31 +1,24 @@
 <script lang="ts">
-	import type { PageData } from "./$types"
-	import Main from "$lib/layouts/main.svelte"
 	import ProfilePreview from "$lib/components/profile-preview.svelte"
 	import Column from "$lib/layouts/column.svelte"
-	import EntryPreview from "$lib/components/entry-preview/"
+	import Entry from "$lib/components/entry-preview.svelte"
 
-	export let data: PageData
-	let { profile, user, entries } = data.props
+	import Json from "$lib/utils/json.svelte"
+
+	const { data } = $props()
+	const { profile, user, blogs, entries } = data
 </script>
 
-<Main>
-	<div class="flex flex-col sm:flex-row gap-8">
-		<div class="mx-auto sm:mx-0">
-			<ProfilePreview {profile} {user} />
-		</div>
+<svelte:head>
+	<title>{profile.displayName}</title>
+</svelte:head>
 
-		<div class="flex flex-col gap-4">
-			<h2 class="text-2xl sm:text-4xl font-semibold leading-tight text-zinc-900">Blog Posts</h2>
-			<Column>
-				{#each entries as { title, blogName, blogSlug, postSlug, createdAt }}
-					<EntryPreview>
-						<EntryPreview.Blog slot="blog" {blogName} {blogSlug} />
-						<EntryPreview.Title slot="title" {title} {blogSlug} {postSlug} />
-						<EntryPreview.Date slot="date" date={createdAt} />
-					</EntryPreview>
-				{/each}
-			</Column>
-		</div>
-	</div>
-</Main>
+<div class="flex flex-col gap-3 mx-auto items-center">
+	<ProfilePreview {profile} {user} />
+
+	<Column>
+		{#each entries as entry}
+			<Entry {entry} omit={["author"]} />
+		{/each}
+	</Column>
+</div>
--- frontend/src/routes/(pages)/profiles/[username]/+page.ts
+++ frontend/src/routes/(pages)/profiles/[username]/+page.ts
@@ -1,38 +1,45 @@
+import { PUBLIC_API } from "$env/static/public"
 import type { PageLoad } from "./$types"
 import type { User, Blog, Profile, Entry } from "$lib/types"
 import { error } from "@sveltejs/kit"
 
 export const load: PageLoad = async ({ fetch, params }) => {
-	const userResp = await fetch(`/api/users/${params.username}`)
-	if (!userResp.ok) {
-		throw error(userResp.status, "User not found")
+	async function fetchUser(): Promise<User> {
+		const userResp = await fetch(`${PUBLIC_API}/users/${params.username}`)
+		if (!userResp.ok) {
+			throw error(userResp.status, "User not found")
+		}
+		return await userResp.json()
 	}
-	const user: User = await userResp.json()
 
-	const profileResp = await fetch(`/api/profiles/${params.username}`)
-	if (!profileResp.ok) {
-		throw error(profileResp.status, "Profile not found")
+	async function fetchProfile(): Promise<Profile> {
+		const profileResp = await fetch(`${PUBLIC_API}/profiles/${params.username}`)
+		if (!profileResp.ok) {
+			throw error(profileResp.status, "Profile not found")
+		}
+		return await profileResp.json()
 	}
-	const profile: Profile = await profileResp.json()
 
-	const blogsResp = await fetch(`/api/profiles/${params.username}/blogs`)
-	if (!blogsResp.ok) {
-		throw error(blogsResp.status, "Blogs not found")
+	async function fetchBlogs(): Promise<Blog[]> {
+		const blogsResp = await fetch(`/api/profiles/${params.username}/blogs`)
+		if (!blogsResp.ok) {
+			throw error(blogsResp.status, "Blogs not found")
+		}
+		return await blogsResp.json()
 	}
-	const blogs: Blog[] = await blogsResp.json()
 
-	const entriesResp = await fetch(`/api/profiles/${params.username}/entries`)
-	if (!entriesResp.ok) {
-		throw error(entriesResp.status, "Entries not found")
+	async function fetchEntries(): Promise<Entry[]> {
+		const entriesResp = await fetch(`/api/profiles/${params.username}/entries`)
+		if (!entriesResp.ok) {
+			throw error(entriesResp.status, "Entries not found")
+		}
+		return await entriesResp.json()
 	}
-	const entries: Entry[] = await entriesResp.json()
 
 	return {
-		props: {
-			profile,
-			blogs,
-			user,
-			entries
-		}
+		user: await fetchUser(),
+		profile: await fetchProfile(),
+		blogs: await fetchBlogs(),
+		entries: await fetchEntries()
 	}
 }
