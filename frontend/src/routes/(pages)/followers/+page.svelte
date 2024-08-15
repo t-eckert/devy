6dddac9c9c63c9df6d97b7a287ad246871efaeee
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-08-14 23:50:58 -0400

    routes: remove followers and following

--- frontend/src/routes/(pages)/followers/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import H1 from "$lib/elements/h1.svelte"
-</script>
-
-<H1>Followers</H1>
--- frontend/src/routes/(pages)/following/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import H1 from "$lib/elements/h1.svelte"
-</script>
-
-<H1>Following</H1>
