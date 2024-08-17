b63520d38c4cc486ac3bab44e2dbdf27b431e961
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-08-17 16:18:18 -0400

    markdown-renderer: make the codeblock a little bigger

--- frontend/src/lib/markdown/renderers/pre.svelte
+++ frontend/src/lib/markdown/renderers/pre.svelte
@@ -1,1 +1,1 @@
-<pre class="bg-zinc-900 text-zinc-50 p-3 rounded-md overflow-auto mb-2 text-xs"><slot /></pre>
+<pre class="bg-zinc-900 text-zinc-50 p-3 rounded-md overflow-auto mb-2 text-sm"><slot /></pre>
