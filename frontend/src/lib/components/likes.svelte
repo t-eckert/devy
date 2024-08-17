025991a9948d6affb3e22a3463c1ebe258d43727
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-08-17 13:48:41 -0400

    likes: disable button when not signedIn

--- frontend/src/lib/components/likes.svelte
+++ frontend/src/lib/components/likes.svelte
@@ -2,8 +2,10 @@
 	import Heart from "$lib/icons/heart.svelte"
 	import HeartFilled from "$lib/icons/heart-filled.svelte"
 	import { getLikesState } from "$lib/state/likes.svelte"
+	import { getSessionState } from "$lib/state/session.svelte"
 
 	const likesState = getLikesState()
+	const sessionState = getSessionState()
 
 	const { postId, likes }: { postId: string; likes: number } = $props()
 
@@ -16,20 +18,29 @@
 	}
 </script>
 
-<button class="mr-2 text-sm flex flex-row items-center justify-start gap-1.5 group" {onclick}>
-	{#if isLiked}
-		<div
-			class="w-4 aspect-square text-stone-900 group-hover:-translate-y-1.5 transition-all group-hover:text-red-500 group-hover:rotate-12 group-hover:scale-110"
-		>
-			<HeartFilled />
-		</div>
-	{:else}
-		<div
-			class="w-4 aspect-square text-stone-500 group-hover:-translate-y-1.5 transition-all group-hover:text-red-500 group-hover:rotate-12 group-hover:scale-110"
-		>
+{#if sessionState.signedIn}
+	<button class="mr-2 text-sm flex flex-row items-center justify-start gap-1.5 group" {onclick}>
+		{#if isLiked}
+			<div
+				class="w-4 aspect-square text-stone-900 group-hover:-translate-y-1.5 transition-all group-hover:text-red-500 group-hover:rotate-12 group-hover:scale-110"
+			>
+				<HeartFilled />
+			</div>
+		{:else}
+			<div
+				class="w-4 aspect-square text-stone-500 group-hover:-translate-y-1.5 transition-all group-hover:text-red-500 group-hover:rotate-12 group-hover:scale-110"
+			>
+				<Heart />
+			</div>
+		{/if}
+
+		<span class="w-8 flex flex-row justify-start text-stone-700 font-medium">{count}</span>
+	</button>
+{:else}
+	<div class="mr-2 text-sm flex flex-row items-center justify-start gap-1.5 select-none">
+		<div class="w-4 aspect-square text-stone-500">
 			<Heart />
 		</div>
-	{/if}
-
-	<span class="w-8 flex flex-row justify-start text-stone-700 font-medium">{count}</span>
-</button>
+		<span class="w-8 flex flex-row justify-start text-stone-700 font-medium">{count}</span>
+	</div>
+{/if}
