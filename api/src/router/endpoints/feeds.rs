4ed0781a8869b84e4fd433c2c313e00a7570a233
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-08-17 13:45:09 -0400

    feeds: add following

--- api/src/router/endpoints/feeds.rs
+++ api/src/router/endpoints/feeds.rs
@@ -1,12 +1,20 @@
-use crate::router::error::Result;
-use axum::{extract::State, routing::get, Json};
-use lib::{db::feed, entities::Feed, store::Store};
+use crate::router::{error::Result, middleware::auth};
+use axum::{extract::State, routing::get,middleware, Extension,  Json};
+use lib::{db::feed, entities::Feed, store::Store, token::Session};
 
 pub fn router(store: Store) -> axum::Router<Store> {
-    axum::Router::new()
-        .route("/feeds/recent", get(get_recent))
-        .route("/feeds/popular", get(get_popular))
-        .with_state(store)
+    let open =
+        axum::Router::new()
+            .route("/feeds/recent", get(get_recent))
+            .route("/feeds/popular", get(get_popular))
+            .with_state(store.clone());
+
+    let protected =        axum::Router::new()
+            .route("/feeds/following", get(get_following))
+            .layer(middleware::from_fn_with_state(store.clone(), auth))
+            .with_state(store);
+
+    axum::Router::new().merge(open).merge(protected)
 }
 
 // GET /feeds/recent
@@ -17,4 +25,12 @@ async fn get_recent(State(store): State<Store>) -> Result<Json<Feed>> {
 // GET /feeds/popular
 async fn get_popular(State(store): State<Store>) -> Result<Json<Feed>> {
     Ok(Json(feed::get_popular(&store.db).await?))
+}
+
+// Get /feeds/following
+async fn get_following(
+    Extension(session): Extension<Session>,
+    State(store): State<Store>) -> Result<Json<Feed>> {
+    let profile_id = session.profile_id;
+    Ok(Json(feed::get_following(&store.db, profile_id).await?))
 }
--- lib/src/db/feed/actions.rs
+++ lib/src/db/feed/actions.rs
@@ -1,9 +1,11 @@
+use uuid::Uuid;
+
 use crate::db::{error::Result, Database};
 use crate::entities::{Entry, Feed, FeedConfig};
 
 pub async fn get_recent(db: &Database) -> Result<Feed> {
     Ok(Feed {
-        feed_config: FeedConfig::new("recent", "Recent Posts"),
+        feed_config: FeedConfig::new("recent", "Recent"),
         entries: sqlx::query_file_as!(Entry, "src/db/feed/queries/get_recent.sql")
             .fetch_all(db)
             .await?,
@@ -12,8 +14,17 @@ pub async fn get_recent(db: &Database) -> Result<Feed> {
 
 pub async fn get_popular(db: &Database) -> Result<Feed> {
     Ok(Feed {
-        feed_config: FeedConfig::new("popular", "Popular Posts"),
+        feed_config: FeedConfig::new("popular", "Popular"),
         entries: sqlx::query_file_as!(Entry, "src/db/feed/queries/get_popular.sql")
+            .fetch_all(db)
+            .await?,
+    })
+}
+
+pub async fn get_following(db: &Database, profile_id: Uuid) -> Result<Feed> {
+    Ok(Feed {
+        feed_config: FeedConfig::new("following", "Following"),
+        entries: sqlx::query_file_as!(Entry, "src/db/feed/queries/get_following.sql")
             .fetch_all(db)
             .await?,
     })
