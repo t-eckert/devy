751159f400ec52585daed6fde5c400b64c234c4e
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-08-21 22:21:55 -0400

    tests: add test module to lib

---
+++ lib/src/test/test_store.rs
@@ -0,0 +1,29 @@
+use crate::store::Store;
+use crate::db::DBConn;
+
+pub struct Options {
+    pub db_conn: DBConn,
+}
+
+pub fn new_test_store(opts: Options) -> Store {
+    // TODO: This will not really work for non-database use-cases. Come back to this.
+    Store::new(
+        opts.db_conn,
+        crate::auth::Client::new(
+            "client_id".to_string(),
+            "client_secret".to_string(),
+            "callback_url".to_string(),
+            "".to_string(),
+            "".to_string(),
+            include_bytes!("test_private_key.pem"),
+            include_bytes!("test_public_key.pem"),
+        ),
+        crate::uploader::Uploader::new(
+            crate::uploader::Git::new("/tmp").unwrap(),
+        ),
+        crate::github::Client::new(
+            "client_id",
+            "private_key",
+        ),
+    )
+}
