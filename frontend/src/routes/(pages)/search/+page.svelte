977fdee81ae949019683586cf474b5b0b02645b1
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-09-08 19:35:21 -0400

    search: create a dummy search page

---
+++ frontend/src/routes/(pages)/search/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Placeholder from "$lib/components/placeholder.svelte"
+	import { PUBLIC_API } from "$env/static/public"
+
+	let query = $state("")
+	let result = $state("result")
+
+	async function search() {
+		const resp = await fetch(`${PUBLIC_API}/search?query=${query}`)
+		result = await resp.text()
+	}
+</script>
+
+<Placeholder>Search</Placeholder>
+
+<input type="text" class="border border-stone-200 rounded" bind:value={query} oninput={search} />
+<div>{result}</div>
