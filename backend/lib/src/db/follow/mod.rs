416d56b0925784e6d2402f6dfd28640da54ef841
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-09-17 21:19:58 -0400

    follow: move db code over to follow repository

--- backend/lib/src/db/follow/mod.rs
+++
@@ -1,5 +0,0 @@
-mod relation;
-mod actions;
-
-pub use relation::Follow;
-pub use actions::*;
--- backend/lib/src/db/mod.rs
+++ backend/lib/src/db/mod.rs
@@ -4,7 +4,6 @@ mod error;
 pub use database::{connect, Conn, DBConn, Database, MIGRATOR};
 pub use error::{Error, Result};
 
-pub mod follow;
 pub mod like;
 pub mod profile;
 pub mod repo;
