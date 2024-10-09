f04fe1e924229f5d5c136fb5603630b097ae279f
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-10-08 21:51:44 -0400

    auth: fix redirect after sign-out

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -3,11 +3,11 @@ import type { Handle } from "@sveltejs/kit"
 export const handle: Handle = async ({ event, resolve }) => {
   {
     const err = event.url.searchParams.get("error")
-    console.log("error", err)
+    if (err) console.log("error", err)
 
     const token = event.url.searchParams.get("token")
-    console.log("token", token)
     if (token) {
+      console.log("token", token)
       event.cookies.set("token", token, { path: "/", maxAge: 60 * 60 * 24 * 365 })
       event.url.searchParams.delete("token")
     }
