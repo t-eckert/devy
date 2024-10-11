5bc7c433955f19890ddb0e4284498acd90a2a08f
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-10-10 22:24:06 -0400

    og: reference og image

---
+++ frontend/src/lib/components/og-header.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	interface Props {
+		title: string
+	}
+
+	const { title }: Props = $props()
+</script>
+
+<meta property="og:title" content={title} />
+<meta property="og:image" content="https://devy.page/og-img.png" />
--- frontend/src/routes/(pages)/+layout.svelte
+++ frontend/src/routes/(pages)/+layout.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
 	import Analytics from "$lib/utils/analytics.svelte"
 	import Main from "$lib/layouts/main.svelte"
+	import OGHeader from "$lib/components/og-header.svelte"
 
 	import Header from "./header.svelte"
 	import Footer from "./footer.svelte"
 </script>
+
+<svelte:head>
+	<title>Devy: git push blog</title>
+	<OGHeader title="Devy: git push blog" />
+</svelte:head>
 
 <Analytics />
 <div class="min-h-screen">
