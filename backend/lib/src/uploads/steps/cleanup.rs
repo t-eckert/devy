9f40bc20bea9bb8aa64cac938273a6cccae973a8
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-10-21 22:06:13 -0400

    uploader: add cleanup step

--- backend/lib/src/uploads/steps/cleanup.rs
+++ backend/lib/src/uploads/steps/cleanup.rs
@@ -4,8 +4,18 @@ use crate::{
     uploads::{Error, Result, Status, Upload},
 };
 
+/// The cleanup step removes the temporary directory created for the upload.
 pub async fn cleanup(db_conn: &Conn, mut upload: Upload) -> Result<Upload> {
     dbg!("cleanup");
+
+    match std::fs::remove_dir_all(&upload.dir()).map_err(|_| Error::CleanupFailure) {
+        Ok(_) => {}
+        Err(err) => {
+            upload.set_status(Status::FAILED);
+            upload.append_log(&format!("ERROR: Could not remove directory: {}", err));
+            return Ok(upload);
+        }
+    }
 
     upload.set_status(Status::DONE);
     Ok(upload)
