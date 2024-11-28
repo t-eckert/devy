33d0983033aa8b6869cedef6defe20a8a1821cd5
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-11-28 12:14:37 -0500

    app: add layouts

---
+++ apps/devy-app/src/lib/layouts/bounded.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { Snippet } from "svelte"
+
+	let { children }: { children: Snippet } = $props()
+</script>
+
+<div class="mx-auto w-full max-w-6xl gap-4 px-2 sm:gap-2">
+	{@render children()}
+</div>
---
+++ apps/devy-app/src/lib/layouts/centered.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { Snippet } from "svelte"
+
+	let { children }: { children: Snippet } = $props()
+</script>
+
+<div class="flex h-[80vh] items-center justify-center">
+	{@render children()}
+</div>
