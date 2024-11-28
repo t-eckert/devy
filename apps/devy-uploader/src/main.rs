ef2c421d12019cc12647093ac08027f820fbe93b
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-11-28 12:10:58 -0500

    uploader: add basic get/post handling

--- apps/devy-uploader/src/main.rs
+++ apps/devy-uploader/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::{get, post};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let router = axum::Router::new()
+        .route("/", post(|| async { "Received POST request" }))
+        .route("/", get(|| async { "Received GET request" }));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000").await?;
+    Ok(axum::serve(listener, router).await?)
 }
