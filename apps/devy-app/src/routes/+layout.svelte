72e614377124a0e40b38e9b470ca909ce3d9e322
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-11-28 23:18:22 -0500

    app: add fonts

--- apps/devy-app/src/routes/+layout.svelte
+++ apps/devy-app/src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import "../app.css";
+	import "@fontsource-variable/inter";
+	import "@fontsource-variable/playfair";
+	import "@fontsource-variable/playfair-display";
 	import Main from "$lib/layouts/main.svelte";
 	import Bounded from "$lib/layouts/bounded.svelte";
 	import Header from "./header.svelte";
