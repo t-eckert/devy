62080916e8cc796ea6b2407703ba3ad5e36b8b4a
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-11-28 21:40:13 -0500

    app: add header

--- apps/devy-app/src/routes/+layout.svelte
+++ apps/devy-app/src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import "../app.css";
 	import Main from "$lib/layouts/main.svelte";
+	import Header from "./header.svelte";
 	import Footer from "./footer.svelte";
 
 	let { children } = $props();
 </script>
 
 <Main>
+	<Header />
 	{@render children()}
 </Main>
 <Footer />
---
+++ apps/devy-app/src/routes/header.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Bounded from "$lib/layouts/bounded.svelte";
+	import Logo from "$lib/components/logo.svelte";
+</script>
+
+<div class="mb-4 border-b border-b-stone-200 py-1">
+	<Bounded>
+		<div class="flex w-full items-center justify-between">
+			<a href="/"><Logo height={18} /></a>
+		</div>
+	</Bounded>
+</div>
