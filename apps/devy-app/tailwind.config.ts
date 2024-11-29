8aaa93e4a561a2d98594c9cc500a9c1170424b99
Thomas Eckert <thomas.james.eckert@gmail.com>
2024-11-28 23:01:50 -0500

    app: configure Tailwind

--- apps/devy-app/tailwind.config.ts
+++ apps/devy-app/tailwind.config.ts
@@ -1,14 +1,24 @@
-import containerQueries from '@tailwindcss/container-queries';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
-import type { Config } from 'tailwindcss';
+import containerQueries from "@tailwindcss/container-queries"
+import forms from "@tailwindcss/forms"
+import typography from "@tailwindcss/typography"
+import type { Config } from "tailwindcss"
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: "class",
+  content: ["./src/**/*.{html,js,svelte,ts}"],
 
-	theme: {
-		extend: {}
-	},
+  theme: {
+    fontFamily: {
+      sans: ["Inter Variable", "sans-serif"],
+      mono: ["IBM Plex Mono", "monospace"],
+      serif: ["Playfair", "serif"]
+    },
+    extend: {
+      fontFamily: {
+        display: ["Playfair Display", "serif"]
+      }
+    }
+  },
 
-	plugins: [typography, forms, containerQueries]
-} satisfies Config;
+  plugins: [typography, forms, containerQueries]
+} as Config
